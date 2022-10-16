tammela/iproute2-p4tc-pub

commit ae29fc4376886001df1250fae677b734337c7dbe

p4tc: Introducing metact
In this initial patch, we introduce metact, which will be the heart of
P4TC. Here we introduce 4 operations: set, act, print and branching

================================SET================================

The set operation allows us to assign values to objects. For now we are
only allowing sets of constant to metadata and metadata to metadata.
In the very near future, we'll be including table keys and header fields
as operands to these set operations, so when explaining the syntax we'll
address table keys and header fields as well.

The set command has the following syntax:

set A B

Where A could be a header field, metadata or a key.
B could be a constant, header field or metadata.

Also, operand A's size must be bigger or equal to operand B's size.

Here are some examples of setting metadata to constants:

tc actions add action metact cmd set \
 metadata.kernel.skbmark constant.bit32.1234

tc actions add action metact cmd set \
 metadata.kernel.tcindex constant.bit32.0x5678

The first example assigns kernel metadata skbmark to 1234 and the second
assigns kernel metadata tcindex to 0x5678

Note that we may specify constants in decimal or hexadecimal format.

Here are some examples of setting metadata to metadata:

tc actions add action metact cmd set \
 metadata.kernel.datalen metadata.kernel.skbmark

tc actions add action metact cmd set \
 metadata.kernel.skbifindex metadata.kernel.skbiif

We can also use user defined metadata in set operations.

For example, if we define the following user metadata

tc p4template create metadata/ptables/mymd type bit32

We could set its value to skbmark, for example

tc actions add action metact cmd set \
 metadata.ptables.mymd metadata.kernel.skbmark

Note that the way to reference user metadata is equivalent
to the way we reference kernel metadata. That is:

METADATA.PIPELINE_NAME.METADATA_NAME

All kernel metadata is stored inside a special pipeline called "kernel".

include/uapi/linux/p4tc.h
@@ -247,6 +247,33 @@ enum {
 };
 #define P4TC_ENTRY_MAX (__P4TC_ENTRY_MAX - 1)
 
+/* Linux system metadata */
+enum {
+	METACT_LMETA_UNSPEC,
+	METACT_LMETA_PKTLEN,	/* u32 */
+	METACT_LMETA_DATALEN,	/* u32 */
+	METACT_LMETA_SKBMARK,	/* u32 */
+	METACT_LMETA_TCINDEX,	/* u16 */
+	METACT_LMETA_SKBHASH,	/* u32 */
+	METACT_LMETA_SKBPRIO,	/* u32 */
+	METACT_LMETA_IFINDEX,	/* s32 */
+	METACT_LMETA_SKBIIF,	/* s32 */
+	METACT_LMETA_PROTOCOL,	/* be16 */
+	METACT_LMETA_PKTYPE,	/* u8:3 */
+	METACT_LMETA_IDF,	/* u8:1 */
+	METACT_LMETA_IPSUM,	/* u8:2 */
+	METACT_LMETA_OOOK,	/* u8:1 */
+	METACT_LMETA_FCLONE,	/* u8:2 */
+	METACT_LMETA_PEEKED,	/* u8:1 */
+	METACT_LMETA_QMAP,	/* u16 */
+	METACT_LMETA_PTYPEOFF,	/* u8 */
+	METACT_LMETA_CLONEOFF,	/* u8 */
+	METACT_LMETA_PTCLNOFF,	/* u16 */
+	METACT_LMETA_DIRECTION, /* u8:1 */
+	__METACT_LMETA_MAX
+};
+#define METACT_LMETA_MAX (__METACT_LMETA_MAX - 1)
+
 #define P4TC_RTA(r)  ((struct rtattr *)(((char *)(r)) + NLMSG_ALIGN(sizeof(struct p4tcmsg))))
 
 #endif

include/uapi/linux/tc_act/tc_metact.h
@@ -1,0 +1,122 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef __UAPI_TC_METACT_H
+#define __UAPI_TC_METACT_H
+
+#include <linux/types.h>
+#include <linux/pkt_cls.h>
+#include <linux/p4tc.h>
+
+/* Operations */
+enum {
+	METACT_OP_UNSPEC,
+	METACT_OP_SET,
+	METACT_OP_ACT,
+	METACT_OP_BEQ,
+	METACT_OP_BNE,
+	METACT_OP_BLT,
+	METACT_OP_BLE,
+	METACT_OP_BGT,
+	METACT_OP_BGE,
+	METACT_OP_PLUS,
+	METACT_OP_PRINT,
+	METACT_OP_TBLAPP,
+	METACT_OP_SNDPORTEGR,
+	__METACT_OP_MAX
+};
+#define METACT_OP_MAX (__METACT_OP_MAX - 1)
+
+/* single operation within TCA_METACT_LIST */
+enum {
+	TCAA_METACT_UNSPEC,
+	TCAA_METACT_OPERATION,	/*struct tca_u_meta_operate */
+	TCAA_METACT_OPER_A,	/*nested TCAA_METACT_OPER_XXX */
+	TCAA_METACT_OPER_B,	/*nested TCAA_METACT_OPER_XXX */
+	TCAA_METACT_OPER_C,	/*nested TCAA_METACT_OPER_XXX */
+	__TCAA_METACT_OPER_MAX
+};
+#define TCAA_METACT_OPER_MAX (__TCAA_METACT_OPER_MAX - 1)
+
+#define METACT_RESULTS_HIT 1
+#define METACT_RESULTS_MISS 2
+
+/* TCAA_METACT_OPERATION */
+struct tca_u_meta_operate {
+	__u16 op_type;		/* METACT_OP_XXX */
+	__u8 op_flags;
+	__u8 op_UNUSED;
+	__u32 op_ctl1;
+	__u32 op_ctl2;
+};
+
+/* Nested TCAA_METACT_OPER_XXX */
+enum {
+	TCAA_METACT_OPND_UNSPEC,
+	TCAA_METACT_OPND_INFO,
+	TCAA_METACT_OPND_PATH,
+	__TCAA_METACT_OPND_MAX
+};
+#define TCAA_METACT_OPND_MAX (__TCAA_METACT_OPND_MAX - 1)
+
+/* Maximum path or value size for an operand */
+#define METACT_PATH_MAX 4
+
+/* operand types */
+enum {
+	METACT_OPER_UNSPEC,
+	METACT_OPER_CONST,
+	METACT_OPER_META,
+	METACT_OPER_ACTID,
+	METACT_OPER_TBL,
+	METACT_OPER_KEY,
+	METACT_OPER_RES,
+	METACT_OPER_HDRFIELD,
+	METACT_OPER_PARAM,
+	METACT_OPER_DEV,
+	__METACT_OPER_MAX
+};
+
+#define METACT_MAX_OPER_PATH_LEN 32
+
+#define METACT_OPER_MAX (__METACT_OPER_MAX - 1)
+/* TCAA_METACT_OPER_INFO operand*/
+struct tca_u_meta_operand {
+	__u32 immedv;		/* immediate value, otherwise stored in
+				 * TCAA_METACT_OPND_PATH
+				 */
+	__u32 immedv2;
+	__u32 pipeid;		/* 0 for kernel-global */
+	__u8 oper_type;		/* METACT_OPER_XXX */
+	__u8 oper_datatype;	/* T_XXX */
+	__u8 oper_cbitsize;	/* Size of container, u8 = 8, etc
+				 * Useful for a type that is not atomic
+				 */
+	__u8 oper_startbit;
+	__u8 oper_endbit;
+	__u8 oper_flags;
+};
+
+/* operand flags */
+#define DATA_IS_IMMEDIATE ((__u8)BIT(0)) /* data is held as immediate value */
+#define DATA_IS_RAW ((__u8)BIT(1))	 /* bitXX datatype, not intepreted by kernel */
+#define DATA_IS_SLICE ((__u8)BIT(2))	 /* bitslice in a container, not intepreted
+					  * by kernel
+					  */
+/* TCA_METACT_PARMS */
+struct tc_metact {
+	tc_gen;
+};
+
+enum {
+	TCA_METACT_UNSPEC,
+	TCA_METACT_TM,
+	TCA_METACT_PARMS,
+	TCA_METACT_LIST,
+	TCA_METACT_ACT_NAME,
+	TCA_METACT_PAD,
+	__TCA_METACT_MAX
+};
+#define TCA_METACT_MAX (__TCA_METACT_MAX - 1)
+
+#define TCA_METACT_LIST_MAX 32
+
+#endif

tc/p4_tc_template.c
@@ -23,6 +23,7 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <dlfcn.h>
+#include <linux/tc_act/tc_metact.h>
 
 #include "utils.h"
 #include "tc_common.h"
@@ -30,6 +31,282 @@
 #include "p4tc_common.h"
 #include "p4_types.h"
 #include "p4tc_introspection.h"
+
+static struct hlist_head kernel_metadata_list = {};
+
+static struct p4_metat_s pktlen_meta = {
+	.id = METACT_LMETA_PKTLEN,
+	.containid = P4T_U32,
+	.startbit = 0,
+	.endbit = 31,
+	.name = "pktlen",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s datalen_meta = {
+	.id = METACT_LMETA_DATALEN,
+	.containid = P4T_U32,
+	.startbit = 0,
+	.endbit = 31,
+	.name = "datalen",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbmark_meta = {
+	.id = METACT_LMETA_SKBMARK,
+	.containid = P4T_U32,
+	.startbit = 0,
+	.endbit = 31,
+	.name = "skbmark",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s tcindex_meta = {
+	.id = METACT_LMETA_TCINDEX,
+	.containid = P4T_U16,
+	.startbit = 0,
+	.endbit = 15,
+	.name = "tcindex",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbhash_meta = {
+	.id = METACT_LMETA_SKBHASH,
+	.containid = P4T_U32,
+	.startbit = 0,
+	.endbit = 31,
+	.name = "skbhash",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbprio_meta = {
+	.id = METACT_LMETA_SKBPRIO,
+	.containid = P4T_U32,
+	.startbit = 0,
+	.endbit = 31,
+	.name = "skbprio",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s ifindex_meta = {
+	.id = METACT_LMETA_IFINDEX,
+	.containid = P4T_S32,
+	.startbit = 0,
+	.endbit = 31,
+	.name = "ifindex",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s iif_meta = {
+	.id = METACT_LMETA_SKBIIF,
+	.containid = P4T_S32,
+	.startbit = 0,
+	.endbit = 31,
+	.name = "iif",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s protocol_meta = {
+	.id = METACT_LMETA_PROTOCOL,
+	.containid = P4T_BE16,
+	.startbit = 0,
+	.endbit = 15,
+	.name = "skbproto",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbptype_meta = {
+	.id = METACT_LMETA_PKTYPE,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 2,
+	.name = "skbptype",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbidf_meta = {
+	.id = METACT_LMETA_IDF,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 0,
+	.name = "skbidf",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbipsum_meta = {
+	.id = METACT_LMETA_IPSUM,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 1,
+	.name = "skbipsum",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbfclon_meta = {
+	.id = METACT_LMETA_FCLONE,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 1,
+	.name = "skbfclon",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbpeek_meta = {
+	.id = METACT_LMETA_PEEKED,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 0,
+	.name = "skbpeek",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skboook_meta = {
+	.id = METACT_LMETA_OOOK,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 0,
+	.name = "skboook",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbqmap_meta = {
+	.id = METACT_LMETA_QMAP,
+	.containid = P4T_U16,
+	.startbit = 0,
+	.endbit = 15,
+	.name = "skbqmap",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s ptypeoff_meta = {
+	.id = METACT_LMETA_PTYPEOFF,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 7,
+	.name = "ptypeoff",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s cloneoff_meta = {
+	.id = METACT_LMETA_CLONEOFF,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 7,
+	.name = "cloneoff",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s direction_meta = {
+	.id = METACT_LMETA_DIRECTION,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 0,
+	.name = "direction",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s ptclnoff_meta = {
+	.id = METACT_LMETA_PTCLNOFF,
+	.containid = P4T_U16,
+	.startbit = 0,
+	.endbit = 15,
+	.name = "ptclnoff",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+struct p4_metat_s *get_meta_byname(const char *pname, const char *name)
+{
+	struct hlist_node *m, *tmp_child;
+
+	hlist_for_each_safe(m, tmp_child, &kernel_metadata_list) {
+		struct p4_metat_s *meta;
+
+		meta = container_of(m, struct p4_metat_s, hlist);
+		if (strcmp(meta->pname, pname) == 0 &&
+		    strnlen(name, METANAMSIZ) == strnlen(meta->name, METANAMSIZ) &&
+		    strncasecmp(meta->name, name, strlen(name)) == 0)
+			return meta;
+	}
+
+	return NULL;
+}
+
+struct p4_metat_s *get_meta_byid(const __u32 pipeid, const __u32 id)
+{
+	struct hlist_node *m, *tmp_child;
+
+	hlist_for_each_safe(m, tmp_child, &kernel_metadata_list) {
+		struct p4_metat_s *meta;
+
+		meta = container_of(m, struct p4_metat_s, hlist);
+		if (pipeid == meta->pipeid &&
+		    id == meta->id)
+			return meta;
+	}
+
+	return NULL;
+}
+
+void register_new_metadata(struct p4_metat_s *meta)
+{
+	hlist_add_head(&meta->hlist, &kernel_metadata_list);
+}
+
+void register_kernel_metadata(void)
+{
+	register_new_metadata(&pktlen_meta);
+	register_new_metadata(&datalen_meta);
+	register_new_metadata(&skbmark_meta);
+	register_new_metadata(&tcindex_meta);
+	register_new_metadata(&skbhash_meta);
+	register_new_metadata(&skbprio_meta);
+	register_new_metadata(&ifindex_meta);
+	register_new_metadata(&iif_meta);
+	register_new_metadata(&skbidf_meta);
+	register_new_metadata(&protocol_meta);
+	register_new_metadata(&skbipsum_meta);
+	register_new_metadata(&skbfclon_meta);
+	register_new_metadata(&skbpeek_meta);
+	register_new_metadata(&skboook_meta);
+	register_new_metadata(&skbqmap_meta);
+	register_new_metadata(&ptypeoff_meta);
+	register_new_metadata(&skbptype_meta);
+	register_new_metadata(&cloneoff_meta);
+	register_new_metadata(&ptclnoff_meta);
+	register_new_metadata(&direction_meta);
+}
+
+void unregister_kernel_metadata(void)
+{
+	struct hlist_node *n, *tmp_meta;
+
+	hlist_for_each_safe(n, tmp_meta, &kernel_metadata_list) {
+		struct p4_metat_s *meta;
+
+		meta = container_of(n, struct p4_metat_s, hlist);
+		hlist_del(&meta->hlist);
+	}
+}
 
 static int try_strncpy(char *dest, const char *src, size_t max_len)
 {

tc/Makefile
@@ -56,6 +56,7 @@ TCMODULES += m_tunnel_key.o
 TCMODULES += m_sample.o
 TCMODULES += m_ct.o
 TCMODULES += m_gate.o
+TCMODULES += m_metact.o
 TCMODULES += p_ip.o
 TCMODULES += p_ip6.o
 TCMODULES += p_icmp.o

tc/p4tc_common.h
@@ -47,6 +47,16 @@ struct p4_param_s {
 
 void parse_path(char *path, char **p4tcpath);
 int get_obj_type(const char *str_obj_type);
+struct p4_metat_s *get_meta_byname(const char *pname, const char *name);
+struct p4_metat_s *get_meta_byid(const __u32 pipeid, const __u32 id);
+void register_kernel_metadata(void);
+void unregister_kernel_metadata(void);
+void register_new_metadata(struct p4_metat_s *meta);
+void unregister_metadata(struct p4_metat_s *meta);
+int parse_commands(struct action_util *a, int *argc_p, char ***argv_p);
+int add_commands(struct nlmsghdr *n, int ins_cnt, int tca_id);
+int print_metact_cmds(FILE *f, struct rtattr *arg);
+int print_metact(struct action_util *au, FILE *f, struct rtattr *arg);
 
 #define STR_IS_EMPTY(str) ((str)[0] == '\0')
 

commit 26da41eb25501906b6812e7f31bbfc4bb93325a2

p4tc: Add metact commands to action templates and params to metact
This is the first step to migrating metact to action templates.

This commit changes action templates to, instead of having their "code"
represented by TC actions, have their "code" represented by metact
commands. After some delibaration and analysis we concluded that it's
architectually cleaner to have metact commands instead of TC actions,
since we were already planning on allowing action templates to only call
metact actions.
It also has a performance advantage, since with TC actions we'd have
every instance of the action template running the same TC actions with
the same index, which would cause locks dispute and consequently
performance penalties.

Fixes: ae29fc437688

Co-developed-by: Victor Nogueira <victor@mojatatu.com>
Co-developed-by: Jamal Hadi Salim <jhs@mojatatu.com>
Co-developed-by: Pedro Tammela <pctammela@mojatatu.com>
Signed-off-by: Victor Nogueira <victor@mojatatu.com>
Signed-off-by: Jamal Hadi Salim <jhs@mojatatu.com>
Signed-off-by: Pedro Tammela <pctammela@mojatatu.com>

include/uapi/linux/p4tc.h
@@ -200,7 +200,7 @@ enum {
 	P4TC_ACT_PARMS, /* nested params */
 	P4TC_ACT_OPT, /* action opt */
 	P4TC_ACT_TM, /* action tm */
-	P4TC_ACT_LIST, /* actions list */
+	P4TC_ACT_METACT_LIST, /* command list */
 	P4TC_ACT_ACTIVE, /* u8 */
 	P4TC_ACT_PAD,
 	__P4TC_ACT_MAX

include/uapi/linux/pkt_cls.h
@@ -141,6 +141,7 @@ enum tca_id {
 	TCA_ID_CT,
 	TCA_ID_GATE,
 	TCA_ID_DYN,
+	TCA_ID_METACT,
 	/* other actions go here */
 	__TCA_ID_MAX = 1023
 };

include/uapi/linux/tc_act/tc_metact.h
@@ -112,6 +112,7 @@ enum {
 	TCA_METACT_PARMS,
 	TCA_METACT_LIST,
 	TCA_METACT_ACT_NAME,
+	TCA_METACT_ACT_PARMS,
 	TCA_METACT_PAD,
 	__TCA_METACT_MAX
 };

tc/m_dyna.c
@@ -238,11 +238,6 @@ int parse_dyna(int *argc_p, char ***argv_p, bool in_act, char *pname,
 					fprintf(stderr, "Unknown state\n");
 					goto err_out;
 				}
-			} else if (strcmp(*argv, "action") == 0) {
-				if (parse_action(&argc, &argv, P4TC_ACT_LIST, n)) {
-					fprintf(stderr, "Illegal action\"\n");
-					return -1;
-				}
 			} else {
 				break;
 			}
@@ -376,64 +371,8 @@ int print_dyna_parms(struct rtattr *arg, FILE *f)
 	return 0;
 }
 
-static int print_dyna(struct action_util *au, FILE *f, struct rtattr *arg)
-{
-	FILE *fp = (FILE *)arg;
-	struct rtattr *tb[P4TC_ACT_MAX + 1];
-	struct tc_act_dyna *opt;
-
-	parse_rtattr_nested(tb, P4TC_ACT_MAX, arg);
-
-	if (tb[P4TC_ACT_NAME]) {
-		const char *name = RTA_DATA(tb[P4TC_ACT_NAME]);
-
-		print_string(PRINT_ANY, "kind", "%s ", name);
-		print_nl();
-	}
-
-	if (tb[P4TC_ACT_OPT] == NULL) {
-		fprintf(stderr, "Missing dyna parameters\n");
-		return -1;
-	}
-	opt = RTA_DATA(tb[P4TC_ACT_OPT]);
-
-	print_string(PRINT_FP, NULL, "%s\t", _SL_);
-	print_action_control(f, "action ", opt->action, " ");
-	print_nl();
-
-	if (tb[P4TC_ACT_PARMS]) {
-		print_string(PRINT_FP, NULL, "\t%s\n", "params: ");
-		open_json_array(PRINT_JSON, "params");
-		print_dyna_parms(tb[P4TC_ACT_PARMS], f);
-		close_json_array(PRINT_JSON, NULL);
-	}
-
-	if (tb[P4TC_ACT_LIST]) {
-		print_nl();
-		print_string(PRINT_FP, NULL, "    Action list:\n", NULL);
-		tc_print_action(fp, tb[P4TC_ACT_LIST], 0);
-	}
-
-	print_nl();
-	print_uint(PRINT_ANY, "index", "\t index %u", opt->index);
-	print_int(PRINT_ANY, "ref", " ref %d", opt->refcnt);
-	print_int(PRINT_ANY, "bind", " bind %d", opt->bindcnt);
-
-	if (show_stats) {
-		if (tb[P4TC_ACT_TM]) {
-			struct tcf_t *tm = RTA_DATA(tb[P4TC_ACT_TM]);
-
-			print_tm(f, tm);
-		}
-	}
-
-	print_nl();
-
-	return 0;
-}
-
 struct action_util dyna_action_util = {
 	.id = "dyna",
 	.parse_aopt = parse_dyna_cb,
-	.print_aopt = print_dyna,
+	.print_aopt = print_metact,
 };

tc/p4_tc_template.c
@@ -552,7 +552,6 @@ static int print_table_instance_flush(struct nlmsghdr *n,
 static int print_action_template(struct nlmsghdr *n, struct rtattr *arg,
 				 __u32 a_id, FILE *f)
 {
-	FILE *fp = (FILE *)arg;
 	struct rtattr *tb[P4TC_ACT_MAX + 1];
 
 	parse_rtattr_nested(tb, P4TC_ACT_MAX, arg);
@@ -572,11 +571,8 @@ static int print_action_template(struct nlmsghdr *n, struct rtattr *arg,
 		close_json_array(PRINT_JSON, NULL);
 	}
 
-	if (tb[P4TC_ACT_LIST]) {
-		print_nl();
-		print_string(PRINT_FP, NULL, "    Action list:\n", NULL);
-		tc_print_action(fp, tb[P4TC_ACT_LIST], 0);
-	}
+	if (tb[P4TC_ACT_METACT_LIST])
+		print_metact_cmds(f, tb[P4TC_ACT_METACT_LIST]);
 
 	return 0;
 }
@@ -979,10 +975,15 @@ static int parse_action_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,
 	char **argv = *argv_p;
 	int argc = *argc_p;
 	__u32 pipeid = 0, actid = 0;
-	int ret = 0;
+	int ret = 0, ins_cnt = 0;
 	char *pname, *actname, *cbname;
+	struct action_util *a;
 	struct rtattr *count;
 	struct rtattr *tail;
+
+	discover_actions();
+
+	a = get_action_byid(TCA_ID_METACT);
 
 	pname = p4tcpath[PATH_PNAME_IDX];
 	cbname = p4tcpath[PATH_CBNAME_IDX];
@@ -998,26 +999,46 @@ static int parse_action_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,
 		return -1;
 	}
 
+	register_kernel_metadata();
+
 	count = addattr_nest(n, MAX_MSG, 1);
 	tail = addattr_nest(n, MAX_MSG, P4TC_PARAMS);
 	while (argc > 0) {
 		if (strcmp(*argv, "pipeid") == 0) {
 			NEXT_ARG();
-			if (get_u32(&pipeid, *argv, 10) < 0)
-				return -1;
+			if (get_u32(&pipeid, *argv, 10) < 0) {
+				ret = -1;
+				goto unregister;
+			}
 		} else if (strcmp(*argv, "actid") == 0) {
 			NEXT_ARG();
-			if (get_u32(&actid, *argv, 10) < 0)
-				return -1;
+			if (get_u32(&actid, *argv, 10) < 0) {
+				ret = -1;
+				goto unregister;
+			}
+		} else if (strcmp(*argv, "cmd") == 0) {
+			ins_cnt = parse_commands(a, &argc, &argv);
+			if (ins_cnt < 0) {
+				ret = -1;
+				goto unregister;
+			}
 		} else {
-			if (parse_dyna(&argc, &argv, false, pname, full_actname, n) < 0)
-				return -1;
+			if (parse_dyna(&argc, &argv, false, pname, full_actname, n) < 0) {
+				ret = -1;
+				goto unregister;
+			}
 		}
 		argv++;
 		argc--;
 	}
 	if (!STR_IS_EMPTY(full_actname))
 		addattrstrz(n, MAX_MSG, P4TC_ACT_NAME, full_actname);
+
+	if (add_commands(n, ins_cnt, P4TC_ACT_METACT_LIST) < 0) {
+		ret = -1;
+		goto unregister;
+	}
+
 	addattr_nest_end(n, tail);
 	if (actid)
 		addattr32(n, MAX_MSG, P4TC_PATH, actid);
@@ -1025,10 +1046,15 @@ static int parse_action_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,
 		*flags |= NLM_F_ROOT;
 	addattr_nest_end(n, count);
 
+	ret = pipeid;
+
+unregister:
+	unregister_kernel_metadata();
+
 	*argc_p = argc;
 	*argv_p = argv;
 
-	return pipeid;
+	return ret;
 }
 
 static int parse_hdrfield_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,