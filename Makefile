tammela/iproute2-p4tc-pub

commit bf33d413a3c77d1ec1c3e68a262bd665e71ac49b

p4tc: Add new introspection functions necessary for metact
Add the following function necessary for instrospection in metact:

- Add p4tc_get_act function to get action id using action name
- Add p4tc_get_metadata function to retrieve all metadata in
  introspection directory
- Add p4tc_get_tables to get table class id using table class name
- Add p4tc_get_params to get all params from a specific action in the
  introspection file

Fixes: 19af70f6d9c1

Signed-off-by: Victor Nogueira <victor@mojatatu.com>

Makefile
@@ -26,6 +26,7 @@ MANDIR?=$(DATADIR)/man
 ARPDDIR?=/var/lib/arpd
 KERNEL_INCLUDE?=/usr/include
 BASH_COMPDIR?=$(DATADIR)/bash-completion/completions
+INTROSPECTION_PATH?=$(CONFDIR)/introspection
 
 # Path to db_185.h include
 DBM_INCLUDE:=$(DESTDIR)/usr/include
@@ -105,6 +106,8 @@ install: all
 	install -m 0755 -d $(DESTDIR)$(HDRDIR)
 	@for i in $(SUBDIRS);  do $(MAKE) -C $$i install; done
 	install -m 0644 $(shell find etc/iproute2 -maxdepth 1 -type f) $(DESTDIR)$(CONFDIR)
+	install -m 0755 -d $(DESTDIR)$(INTROSPECTION_PATH)
+	install -m 0644 $(shell find etc/iproute2/introspection -type f) $(DESTDIR)$(INTROSPECTION_PATH)
 	install -m 0755 -d $(DESTDIR)$(BASH_COMPDIR)
 	install -m 0644 bash-completion/tc $(DESTDIR)$(BASH_COMPDIR)
 	install -m 0644 bash-completion/devlink $(DESTDIR)$(BASH_COMPDIR)

etc/iproute2/introspection/ptables
@@ -1,0 +1,49 @@
+action test 22 1
+param1 bit32 1
+
+action ipv4_forward 22 2
+nhopDstAddr macaddr 1
+numPort bit9 2
+
+action ipv4_drop 22 3
+nhopDstAddr macaddr 1
+
+table tname 22 22
+key srcAddr ipv4 1
+key dstAddr ipv4 1
+
+table tname2 22 2
+key srcPort bit16 1
+key dstPort bit16 1
+
+table tname3 22 3
+key randomKey1 bit8 1
+key randomKey2 bit32 1
+key randomKey3 bit64 1
+
+header ethernet 22 1
+field dstAddr macaddr 1
+field srcAddr macaddr 2
+field type bit16 14
+
+header ipv4 22 2
+field version bit4 15
+field ihl bit4 5
+field tos bit8 6
+field totlen bit16 7
+field id bit16 8
+field flags bit3 9
+field fragoff bit13 10
+field ttl bit8 11
+field proto bit8 12
+field checksum bit16 13
+field srcAddr ipv4 3
+field dstAddr ipv4 4
+
+header tcp 22 3
+field srcPort bit16 16
+field dstPort bit16 17
+
+metadata mymd bit32 22 1
+metadata mytest bit16 22 2
+metadata mytest64 bit64 22 3

etc/iproute2/introspection/simple_l3_pna
@@ -1,0 +1,37 @@
+table tname 22 22
+key srcAddr ipv4 1
+
+table MainControlImpl/l3_match_rx 1 1
+key dstAddr ipv4 1
+
+header ethernet 1 1
+field dstAddr macaddr 1
+field srcAddr macaddr 2
+field type bit16 14
+
+header ipv4 1 2
+field version bit4 15
+field ihl bit4 5
+field tos bit8 6
+field totlen bit16 7
+field id bit16 8
+field flags bit3 9
+field fragoff bit13 10
+field ttl bit8 11
+field proto bit8 12
+field checksum bit16 13
+field srcAddr ipv4 3
+field dstAddr ipv4 4
+
+header tcp 22 3
+field srcPort bit16 16
+field dstPort bit16 17
+
+metadata mymd bit32 1 1
+metadata mytest bit16 1 2
+metadata mytest64 bit64 1 3
+metadata global/drop bit1 1 4
+metadata global/direction bit1 1 5
+
+action pna/send_to_port 1 1
+port dev 1 1

introspection/ptables
@@ -1,46 +1,0 @@
-header ethernet 1
-field dstAddr macaddr 1
-field srcAddr macaddr 2
-field type bit16 3
-
-header ipv4 2
-field version bit4 4
-field ihl bit4 5
-field tos bit8 6
-field totlen bit16 7
-field id bit16 8
-field flags bit3 9
-field fragoff bit13 10
-field ttl bit8 11
-field proto bit8 12
-field checksum bit16 13
-field srcAddr ipv4 14
-field dstAddr ipv4 15
-
-header tcp 3
-field srcPort bit16 16
-field dstPort bit16 17
-
-action test
-param1 u8 1
-param2 u64 2
-
-action ipv4_forward
-nhopDstAddr mac 1
-numPort bit9 2
-
-action ipv4_drop
-nhopDstAddr mac 1
-
-table tname 1
-key srcAddr ipv4
-key dstAddr ipv4
-
-table tname2 2
-key srcPort bit16
-key dstPort bit16
-
-table tname3 3
-key randomKey1 bit8
-key randomKey2 bit32
-key randomKey3 bit64

tc/p4_tc_introspection.c
@@ -14,25 +14,48 @@
  */
 
 #include "p4tc_introspection.h"
-#include "p4_types.h"
 #include <limits.h>
 #include <stdbool.h>
+#include <dirent.h>
+
+static char *get_introspection_path(void)
+{
+	if (!getenv(ENV_VAR))
+		return INTROSPECTION_PATH;
+
+	return getenv(ENV_VAR);
+}
+
+static int concat_pipeline_path(char *path, const char *pname)
+{
+	char *introspection_path = get_introspection_path();
+	int ret;
+
+	ret = snprintf(path, PATH_MAX, "%s/%s", introspection_path,
+		       pname);
+	if (ret < 0) {
+		fprintf(stderr, "Pipeline name too long\n");
+		return -1;
+	}
+
+	return 0;
+}
 
 /* XXX: Introspection will do this properly.
  * For now we are using our own format, see: file introspection/ptables
  */
-static int p4tc_find_obj(FILE *f, const char *obj_name, const __u32 objid,
-			 const char *obj_type)
+static int p4tc_find_obj(FILE *f, const char *obj_name, __u32 *pipeid,
+			 __u32 *objid, const char *obj_type)
 {
 	const size_t tobj_len = strlen(obj_type);
 	char parsed_obj_name[TEMPLATENAMSZ];
 	size_t obj_name_len = 0;
 	char *line = NULL;
 	size_t len = 0;
-	char fmtstr[32] = {0};
-	__u32 parsed_objid;
+	char fmtstr[256] = {0};
+	__u32 parsed_pipeid, parsed_objid;
 
-	if (!obj_name && !objid) {
+	if (!obj_name && !*objid) {
 		fprintf(stderr, "Must specify object name or id\n");
 		return -1;
 	}
@@ -41,7 +64,8 @@ static int p4tc_find_obj(FILE *f, const char *obj_name, const __u32 objid,
 		obj_name_len = strlen(obj_name);
 
 	strcpy(fmtstr, obj_type);
-	strcat(fmtstr, "%s %u");
+	strcat(fmtstr, "%s %u %u");
+
 	while (getline(&line, &len, f) != -1) {
 		char *objline = strstr(line, obj_type);
 
@@ -52,32 +76,45 @@ static int p4tc_find_obj(FILE *f, const char *obj_name, const __u32 objid,
 		if (objline_len < obj_name_len + tobj_len)
 			continue;
 
-		if (sscanf(objline, fmtstr, parsed_obj_name, &parsed_objid) != 2) {
+		if (sscanf(objline, fmtstr, parsed_obj_name, &parsed_pipeid, &parsed_objid) != 3) {
 			fprintf(stderr, "Invalid file format\n");
 			return -1;
 		}
 
-		if (objid && parsed_objid == objid)
+		if ((!*pipeid || (*pipeid && parsed_pipeid == *pipeid)) &&
+		    *objid && parsed_objid == *objid)
 			return 0;
 
-		if (obj_name_len && strncmp(parsed_obj_name, obj_name, obj_name_len) == 0)
+		if (obj_name_len && strncmp(parsed_obj_name, obj_name, obj_name_len) == 0) {
+			if (*pipeid) {
+				if (*pipeid != parsed_pipeid)
+					continue;
+			} else {
+				*pipeid = parsed_pipeid;
+			}
+			if (!*objid)
+				*objid = parsed_objid;
 			return 0;
+		}
 	}
 
 	return -1;
 }
 
-#define find_table(f, obj_name, objid) (p4tc_find_obj(f, obj_name, objid, "table "))
+#define find_table(f, obj_name, pipeid, objid) (p4tc_find_obj(f, obj_name, pipeid, objid, "table "))
 
-#define p4tc_find_header(f, obj_name, objid) (p4tc_find_obj(f, obj_name, objid, "header "))
+#define p4tc_find_header(f, obj_name, pipeid, objid) (p4tc_find_obj(f, obj_name, pipeid, &objid, "header "))
+
+#define p4tc_find_act(f, obj_name, pipeid, objid) (p4tc_find_obj(f, obj_name, pipeid, objid, "action "))
 
 int p4tc_get_header_fields(struct hdrfield fields[], const char *pname,
-			   const char *hdrname)
+			   const char *hdrname, __u32 *pipeid)
 {
 	int i = 0;
 	__u16 hdr_offset = 0;
 	char *line = NULL;
 	char field_name[TEMPLATENAMSZ];
+	__u32 tbcid;
 	char type_str[32];
 	char path[PATH_MAX];
 	char field_str[6];
@@ -85,20 +122,13 @@ int p4tc_get_header_fields(struct hdrfield fields[], const char *pname,
 	FILE *f;
 	int ret;
 
-	if (!getenv(ENV_VAR)) {
-		fprintf(stderr, "INTROSPECTION Environment variable not set\n");
-		return -1;
-	}
-
 	if (!pname) {
 		fprintf(stderr, "Must specify pipeline name for introspection");
 		return -1;
 	}
 
-	if (snprintf(path, PATH_MAX, "%s/%s", getenv(ENV_VAR), pname) >= PATH_MAX) {
-		fprintf(stderr, "Pipeline name too long\n");
+	if (concat_pipeline_path(path, pname) < 0)
 		return -1;
-	}
 
 	f = fopen(path, "r");
 	if (f == NULL) {
@@ -106,7 +136,7 @@ int p4tc_get_header_fields(struct hdrfield fields[], const char *pname,
 		return -1;
 	}
 
-	if (p4tc_find_header(f, hdrname, 0) < 0) {
+	if (p4tc_find_header(f, hdrname, pipeid, tbcid) < 0) {
 		fprintf(stderr, "Unable to find header %s in introspection file\n",
 			hdrname);
 		ret = -1;
@@ -134,8 +164,6 @@ int p4tc_get_header_fields(struct hdrfield fields[], const char *pname,
 			goto out;
 		}
 
-		bitsz = bitsz ? bitsz : p4_type->bitsz;
-
 		fields[i].id = id;
 		fields[i].ty = p4_type;
 
@@ -154,6 +182,195 @@ out:
 
 }
 
+static int p4tc_get_pipeline_metadata(struct p4_metat_s metadata[],
+				      const char *pname, int *i)
+{
+	char *line = NULL;
+	int ret = 0;
+	char m_name[TEMPLATENAMSZ];
+	char type_str[32];
+	char path[PATH_MAX];
+	size_t len;
+	FILE *f;
+
+	if (!pname) {
+		fprintf(stderr, "Must specify pipeline name for introspection");
+		return -1;
+	}
+
+	if (concat_pipeline_path(path, pname) < 0)
+		return -1;
+
+	f = fopen(path, "r");
+	if (f == NULL) {
+		fprintf(stderr, "Unable to open introspection file\n");
+		return -1;
+	}
+
+	while (getline(&line, &len, f) != -1) {
+		__u32 bitsz = 0;
+		struct p4_type_s *p4_type;
+		__u32 m_id, pipeid;
+		int scanned;
+
+		scanned = sscanf(line, "metadata %s %s %u %u[^\n]", m_name,
+				 type_str, &pipeid, &m_id);
+
+		if (scanned != 4)
+			continue;
+
+		p4_type = get_p4type_byarg(type_str, &bitsz);
+		if (!p4_type) {
+			fprintf(stderr, "Invalid P4 type %s\n", type_str);
+			ret = -1;
+			goto out;
+		}
+
+		bitsz = bitsz ? bitsz : p4_type->bitsz;
+
+		metadata[*i].id = m_id;
+		metadata[*i].containid = p4_type->containid;
+		metadata[*i].startbit = p4_type->startbit;
+		metadata[*i].endbit = bitsz + p4_type->startbit - 1;
+		strncpy(metadata[*i].name, m_name, TEMPLATENAMSZ);
+		strncpy(metadata[*i].pname, pname, TEMPLATENAMSZ);
+		metadata[*i].pipeid = pipeid;
+
+		(*i)++;
+	}
+
+out:
+	fclose(f);
+	return ret;
+}
+
+int p4tc_get_act(const char *pname, const char *act_name, __u32 *pipeid,
+		 __u32 *act_id)
+{
+	int ret = 0;
+	char path[PATH_MAX];
+	FILE *f;
+
+	if (concat_pipeline_path(path, pname) < 0)
+		return -1;
+
+	f = fopen(path, "r");
+	if (f == NULL) {
+		fprintf(stderr, "Unable to open introspection file\n");
+		ret = -1;
+		goto out;
+	}
+
+	if (p4tc_find_act(f, act_name, pipeid, act_id) < 0) {
+		fprintf(stderr, "Unable to find action %s\n", act_name);
+		ret = -1;
+		goto out;
+	}
+
+out:
+	fclose(f);
+	return ret;
+}
+
+int p4tc_get_act_params(struct p4_param_s params[], const char *pname,
+			const char *act_name, __u32 *pipeid, __u32 *act_id)
+{
+	char *line = NULL;
+	int ret = 0;
+	int i = 0;
+	char param_name[TEMPLATENAMSZ];
+	char path[PATH_MAX];
+	char type_str[32];
+	size_t len;
+	FILE *f;
+
+	if (concat_pipeline_path(path, pname) < 0)
+		return -1;
+
+	f = fopen(path, "r");
+	if (f == NULL) {
+		fprintf(stderr, "Unable to open introspection file\n");
+		return -1;
+	}
+
+	if (p4tc_find_act(f, act_name, pipeid, act_id) < 0) {
+		fprintf(stderr, "Unable to find action %s\n", act_name);
+		return -1;
+	}
+
+	while (getline(&line, &len, f) != -1) {
+		__u32 bitsz = 0;
+		struct p4_type_s *p4_type;
+		__u32 param_id;
+		int scanned;
+
+		scanned = sscanf(line, "%s %s %u %u[^\n]", param_name,
+				 type_str, pipeid, &param_id);
+
+		if (scanned != 4)
+			continue;
+
+		/* Will have to change this to bit%u or int%u */
+		scanned = sscanf(type_str, "u%u", &bitsz);
+		if (scanned == 1) {
+			p4_type = get_p4type_bysize(bitsz, P4T_TYPE_UNSIGNED);
+			if (!p4_type) {
+				fprintf(stderr, "Invalid P4 type %s\n", type_str);
+				ret = -1;
+				goto out;
+			}
+		} else {
+			p4_type = get_p4type_byname(type_str);
+			if (!p4_type) {
+				fprintf(stderr, "Invalid P4 type %s\n", type_str);
+				ret = -1;
+				goto out;
+			}
+		}
+
+		params[i].id = param_id;
+		params[i].containid = p4_type->containid;
+		strlcpy(params[i].name, param_name, TEMPLATENAMSZ);
+		strlcpy(params[i].pname, pname, TEMPLATENAMSZ);
+		params[i].startbit = p4_type->startbit;
+		params[i].endbit = p4_type->endbit;
+		params[i].pipeid = *pipeid;
+
+		i++;
+	}
+	ret = i;
+
+out:
+	fclose(f);
+	return ret;
+}
+
+int p4tc_get_metadata(struct p4_metat_s metadata[])
+{
+	char *introspection_path = get_introspection_path();
+	struct dirent *de;
+	int ret = 0;
+	int i = 0;
+	DIR *d;
+
+	d = opendir(introspection_path);
+	if (!d) {
+		fprintf(stderr, "Unable to opendir\n");
+		return -1;
+	}
+
+	while ((de = readdir(d)) != NULL) {
+		if (*de->d_name == '.')
+			continue;
+
+		ret = p4tc_get_pipeline_metadata(metadata, de->d_name, &i);
+		if (ret < 0)
+			return ret;
+	}
+
+	return i;
+}
+
 struct hdrfield *p4tc_find_hdrfield(struct hdrfield fields[],
 				    const char *fieldname,
 				    __u32 num_fields)
@@ -168,11 +385,43 @@ struct hdrfield *p4tc_find_hdrfield(struct hdrfield fields[],
 	return NULL;
 }
 
+int p4tc_get_tables(const char *pname, const char *tname, __u32 *pipeid,
+		    __u32 *tbcid)
+{
+	char path[PATH_MAX];
+	FILE *f;
+	int ret;
+
+	if (!getenv(ENV_VAR))
+		ret = snprintf(path, PATH_MAX, "%s/%s", INTROSPECTION_PATH,
+			       pname);
+	else
+		ret = snprintf(path, PATH_MAX, "%s/%s", getenv(ENV_VAR),
+			       pname);
+
+	if (ret < 0) {
+		fprintf(stderr, "Pipeline name too long\n");
+		return -1;
+	}
+
+	f = fopen(path, "r");
+	if (f == NULL) {
+		fprintf(stderr, "Unable to open introspection file\n");
+		return -1;
+	}
+
+	if (find_table(f, tname, pipeid, tbcid) < 0)
+		return -1;
+
+	return 0;
+}
+
 int p4tc_get_table_keys(struct tkey keys[], const char *pname,
 			const char *tname, const __u32 tbc_id)
 {
 	int i = 0;
 	char *line = NULL;
+	__u32 pipeid = 0;
 	char key_name[TABLEKEYNAMSIZ];
 	char type_str[TCLASSNAMSIZ];
 	char path[PATH_MAX];
@@ -181,20 +430,19 @@ int p4tc_get_table_keys(struct tkey keys[], const char *pname,
 	FILE *f;
 	int ret;
 
-	if (!getenv(ENV_VAR))
-		return -1;
-
 	if (!pname)
 		return -1;
 
-	if (snprintf(path, PATH_MAX, "%s/%s", getenv(ENV_VAR), pname) >= PATH_MAX)
+	if (concat_pipeline_path(path, pname) < 0)
 		return -1;
 
 	f = fopen(path, "r");
-	if (f == NULL)
+	if (f == NULL) {
+		fprintf(stderr, "Unable to open introspection file\n");
 		return -1;
+	}
 
-	if (find_table(f, tname, tbc_id) < 0) {
+	if (find_table(f, tname, &pipeid, &tbc_id) < 0) {
 		ret = -1;
 		goto out;
 	}
@@ -203,10 +451,11 @@ int p4tc_get_table_keys(struct tkey keys[], const char *pname,
 		struct p4_type_s *p4_type;
 		int scanned;
 		__u32 bitsz;
+		__u32 key_id;
 
-		scanned = sscanf(line, "%s %s %s[^\n]", key_str, key_name,
-				 type_str);
-		if (scanned != 3)
+		scanned = sscanf(line, "%s %s %s %u[^\n]", key_str, key_name,
+				 type_str, &key_id);
+		if (scanned != 4)
 			break;
 		if (strcmp(key_str, "key") != 0)
 			continue;
@@ -216,6 +465,7 @@ int p4tc_get_table_keys(struct tkey keys[], const char *pname,
 			return -1;
 		strncpy(keys[i].name, key_name, ACTPARAMNAMSIZ);
 		keys[i].type = p4_type;
+		keys[i].key_id = key_id;
 		i++;
 	}
 	ret = i;

tc/p4_tc_template.c
@@ -795,7 +795,8 @@ static int parse_hdrfield_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,
 	hdrname = p4tcpath[PATH_HDRNAME_IDX];
 	fieldname = p4tcpath[PATH_HDRFIELDNAME_IDX];
 	if (pname && hdrname) {
-		num_fields = p4tc_get_header_fields(fields, pname, hdrname);
+		num_fields = p4tc_get_header_fields(fields, pname, hdrname,
+						    &pipeid);
 		if (num_fields < 0)
 			return num_fields;
 	}

tc/p4tc_common.h
@@ -1,4 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _P4TC_COMMON_H_
+#define _P4TC_COMMON_H_
+
+#include "list.h"
+#include "tc_util.h"
+
+#ifndef INTROSPECTION_PATH
+#define INTROSPECTION_PATH "/etc/iproute2/introspection"
+#endif
 
 #define TMPL_ARRAY_START_IDX 1
 #define PATH_OBJ_IDX 0
@@ -15,6 +24,27 @@
 
 #define MAX_PATH_COMPONENTS 5
 
+struct p4_metat_s {
+        __u32 id;
+        int containid;
+	__u8 startbit;
+	__u8 endbit;
+        char name[256];
+	__u32 pipeid;
+	struct hlist_node hlist;
+	char pname[256];
+};
+
+struct p4_param_s {
+	__u32 id;
+	__u32 pipeid;
+	char pname[256];
+        char name[256];
+	int containid;
+	__u8 startbit;
+	__u8 endbit;
+};
+
 void parse_path(char *path, char **p4tcpath);
 int get_obj_type(const char *str_obj_type);
 
@@ -22,3 +52,4 @@ int get_obj_type(const char *str_obj_type);
 
 int concat_cb_name(char *full_name, const char *cbname,
 		   const char *objname, size_t sz);
+#endif

tc/p4tc_introspection.h
@@ -10,6 +10,7 @@
 #include <stdio.h>
 
 #include "p4_types.h"
+#include "p4tc_common.h"
 
 #define ENV_VAR "INTROSPECTION"
 
@@ -17,8 +18,7 @@
 
 struct parse_state;
 
-struct mask_ops {
-	int (*parse)(struct parse_state *state, __u32 *offset,
+struct mask_ops { int (*parse)(struct parse_state *state, __u32 *offset,
 		     const char *argv);
 };
 
@@ -27,6 +27,7 @@ struct tkey {
 	__u8 value[P4TC_MAX_KEYSZ];
 	__u8 mask[P4TC_MAX_KEYSZ];
 	struct p4_type_s *type;
+	__u32 key_id;
 };
 
 int str_to_type(const char *type_str);
@@ -43,9 +44,17 @@ struct hdrfield {
 	__u16 endbit;
 };
 
+int p4tc_get_act(const char *pname, const char *act_name, __u32 *pipeid,
+		 __u32 *act_id);
 struct hdrfield *p4tc_find_hdrfield(struct hdrfield fields[],
 				    const char *fieldname,
 				    __u32 num_fields);
 int p4tc_get_header_fields(struct hdrfield fields[], const char *pname,
-			   const char *hdrname);
+			   const char *hdrname, __u32 *pipeid);
+
+int p4tc_get_metadata(struct p4_metat_s metadata[]);
+int p4tc_get_tables(const char *pname, const char *tname, __u32 *pipeid,
+		    __u32 *tbcid);
+int p4tc_get_act_params(struct p4_param_s params[], const char *pname,
+			const char *act_name, __u32 *pipeid, __u32 *act_id);
 #endif

commit 5933b74a12ccaf2fe1ab97f7430333feab08085a

p4tc: Export INTROSPECTION_PATH variable to C
Fixes: bf33d413a3c7

Signed-off-by: Victor Nogueira <victor@mojatatu.com>

Makefile
@@ -40,7 +40,8 @@ endif
 
 DEFINES+=-DCONFDIR=\"$(CONFDIR)\" \
          -DNETNS_RUN_DIR=\"$(NETNS_RUN_DIR)\" \
-         -DNETNS_ETC_DIR=\"$(NETNS_ETC_DIR)\"
+         -DNETNS_ETC_DIR=\"$(NETNS_ETC_DIR)\" \
+         -DINTROSPECTION_PATH=\"$(INTROSPECTION_PATH)\"
 
 #options for AX.25
 ADDLIB+=ax25_ntop.o