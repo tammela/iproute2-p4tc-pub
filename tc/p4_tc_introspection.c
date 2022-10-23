tammela/iproute2-p4tc-pub

commit 19af70f6d9c1a9b77519a1a06a4a210bd666ccce

p4tc: Add header field create, get, delete, flush and dump
This commit allows control to create, get, delete, flush and dump header
field objects. The created header fields are retrieved at runtime by
the parser. From a control plane interaction, a header field can only be
created once the appropriate parser is instantiated. At runtime, existing
header fields can be referenced for computation reasons from metact:
metact will use header fields to either create lookup keys or edit the
header fields.

Header fields are part of a pipeline and a parser instance and header
fields can only be created in an unsealed pipeline.

To create a header field, the user must issue the following command:

tc p4template create hdrfield/ptables/myparser/ipv4/totlen

where ptables is the name of a pipeline, myparser is a name of a parser
instance, ipv4 is the name of header and totlen is a header field.
Essential header field information, such as length, position and type
are retrieved by consulting a JSON introspection file that will be
generated by the P4 compiler.

To delete a header field, the user must issue the equivalent of the
following command:

tc p4template del hdrfield/ptables/myparser/ipv4/totlen

where ptables is the name of pipeline, myparser is a name of a parser
instance, ipv4 is the name of header and totlen is a header field.

To retrieve meta-information from a header field, such as length,
position and type, the user must issue the equivalent of the following
command:

tc p4template get hdrfield/ptables/myparser/ipv4/totlen

where ptables is the name of pipeline, myparser is a name of a parser
instance, ipv4 is the name of header and totlen is a header field.

The user can also dump all the header fields available in a parser
instance using the following command:

tc p4template get hdrfield/ptables/myparser/

With that, the user will get all the header field names available in a
specific parser instance.

tc/p4_tc_introspection.c
@@ -1,0 +1,167 @@
+/*
+ * p4_tc_introspection.c	P4 TC Introspection
+ *
+ *		This program is free software; you can distribute it and/or
+ *		modify it under the terms of the GNU General Public License
+ *		as published by the Free Software Foundation; either version
+ *		2 of the License, or (at your option) any later version.
+ *
+ * Copyright (c) 2022, Mojatatu Networks
+ * Copyright (c) 2022, Intel Corporation.
+ * Authors:     Jamal Hadi Salim <jhs@mojatatu.com>
+ *              Victor Nogueira <victor@mojatatu.com>
+ *              Pedro Tammela <pctammela@mojatatu.com>
+ */
+
+#include "p4tc_introspection.h"
+#include "p4_types.h"
+#include <limits.h>
+#include <stdbool.h>
+
+/* XXX: Introspection will do this properly.
+ * For now we are using our own format, see: file introspection/ptables
+ */
+static int p4tc_find_obj(FILE *f, const char *obj_name, const __u32 objid,
+			 const char *obj_type)
+{
+	const size_t tobj_len = strlen(obj_type);
+	char parsed_obj_name[TEMPLATENAMSZ];
+	size_t obj_name_len = 0;
+	char *line = NULL;
+	size_t len = 0;
+	char fmtstr[32] = {0};
+	__u32 parsed_objid;
+
+	if (!obj_name && !objid) {
+		fprintf(stderr, "Must specify object name or id\n");
+		return -1;
+	}
+
+	if (obj_name)
+		obj_name_len = strlen(obj_name);
+
+	strcpy(fmtstr, obj_type);
+	strcat(fmtstr, "%s %u");
+	while (getline(&line, &len, f) != -1) {
+		char *objline = strstr(line, obj_type);
+
+		if (!objline)
+			continue;
+		const size_t objline_len = strlen(objline);
+
+		if (objline_len < obj_name_len + tobj_len)
+			continue;
+
+		if (sscanf(objline, fmtstr, parsed_obj_name, &parsed_objid) != 2) {
+			fprintf(stderr, "Invalid file format\n");
+			return -1;
+		}
+
+		if (objid && parsed_objid == objid)
+			return 0;
+
+		if (obj_name_len && strncmp(parsed_obj_name, obj_name, obj_name_len) == 0)
+			return 0;
+	}
+
+	return -1;
+}
+
+#define p4tc_find_header(f, obj_name, objid) (p4tc_find_obj(f, obj_name, objid, "header "))
+
+int p4tc_get_header_fields(struct hdrfield fields[], const char *pname,
+			   const char *hdrname)
+{
+	int i = 0;
+	__u16 hdr_offset = 0;
+	char *line = NULL;
+	char field_name[TEMPLATENAMSZ];
+	char type_str[32];
+	char path[PATH_MAX];
+	char field_str[6];
+	size_t len;
+	FILE *f;
+	int ret;
+
+	if (!getenv(ENV_VAR)) {
+		fprintf(stderr, "INTROSPECTION Environment variable not set\n");
+		return -1;
+	}
+
+	if (!pname) {
+		fprintf(stderr, "Must specify pipeline name for introspection");
+		return -1;
+	}
+
+	if (snprintf(path, PATH_MAX, "%s/%s", getenv(ENV_VAR), pname) >= PATH_MAX) {
+		fprintf(stderr, "Pipeline name too long\n");
+		return -1;
+	}
+
+	f = fopen(path, "r");
+	if (f == NULL) {
+		fprintf(stderr, "Unable to open introspection file\n");
+		return -1;
+	}
+
+	if (p4tc_find_header(f, hdrname, 0) < 0) {
+		fprintf(stderr, "Unable to find header %s in introspection file\n",
+			hdrname);
+		ret = -1;
+		goto out;
+	}
+
+	while (getline(&line, &len, f) != -1) {
+		__u32 bitsz = 0;
+		struct p4_type_s *p4_type;
+		int scanned;
+		__u32 id;
+
+		scanned = sscanf(line, "%s %s %s %u[^\n]", field_str, field_name,
+				 type_str, &id);
+
+		if (scanned != 4)
+			break;
+		if (strcmp(field_str, "field") != 0)
+			continue;
+
+		p4_type = get_p4type_byarg(type_str, &bitsz);
+		if (!p4_type) {
+			fprintf(stderr, "Invalid P4 type %s\n", type_str);
+			ret = -1;
+			goto out;
+		}
+
+		bitsz = bitsz ? bitsz : p4_type->bitsz;
+
+		fields[i].id = id;
+		fields[i].ty = p4_type;
+
+		fields[i].startbit = hdr_offset;
+		fields[i].endbit = fields[i].startbit + bitsz - 1;
+		strcpy(fields[i].name, field_name);
+
+		hdr_offset += bitsz;
+		i++;
+	}
+	ret = i;
+
+out:
+	fclose(f);
+	return ret;
+
+}
+
+struct hdrfield *p4tc_find_hdrfield(struct hdrfield fields[],
+				    const char *fieldname,
+				    __u32 num_fields)
+{
+	int i;
+
+	for (i = 0; i < num_fields; i++) {
+		if (strncmp(fields[i].name, fieldname, TEMPLATENAMSZ) == 0)
+			return &fields[i];
+	}
+
+	return NULL;
+}

include/uapi/linux/p4tc.h
@@ -23,6 +23,7 @@ struct p4tcmsg {
 #define METANAMSIZ TEMPLATENAMSZ
 #define TCLASSNAMSIZ TEMPLATENAMSZ
 #define TINSTNAMSIZ TEMPLATENAMSZ
+#define HDRFIELDNAMSIZ TEMPLATENAMSZ
 
 #define P4TC_TCLASS_FLAGS_KEYSZ 0x01
 #define P4TC_TCLASS_FLAGS_COUNT 0x02
@@ -68,6 +69,7 @@ enum {
 	P4TC_OBJ_META,
 	P4TC_OBJ_TABLE_CLASS,
 	P4TC_OBJ_TABLE_INST,
+	P4TC_OBJ_HDR_FIELD,
 	__P4TC_OBJ_MAX,
 };
 #define P4TC_OBJ_MAX __P4TC_OBJ_MAX
@@ -167,6 +169,22 @@ enum {
 	__P4TC_TINST_MAX
 };
 #define P4TC_TINST_MAX __P4TC_TINST_MAX
+
+struct p4tc_header_field_ty {
+	__u16 startbit;
+	__u16 endbit;
+	__u8  datatype; /* P4T_* */
+};
+
+/* Header field attributes */
+enum {
+	P4TC_HDRFIELD_UNSPEC,
+	P4TC_HDRFIELD_DATA,
+	P4TC_HDRFIELD_NAME,
+	P4TC_HDRFIELD_PARSER_NAME,
+	__P4TC_HDRFIELD_MAX
+};
+#define P4TC_HDRFIELD_MAX (__P4TC_HDRFIELD_MAX - 1)
 
 #define P4TC_RTA(r)  ((struct rtattr *)(((char *)(r)) + NLMSG_ALIGN(sizeof(struct p4tcmsg))))
 

introspection/ptables
@@ -1,0 +1,22 @@
+header ethernet 1
+field dstAddr macaddr 1
+field srcAddr macaddr 2
+field type bit16 3
+
+header ipv4 2
+field version bit4 4
+field ihl bit4 5
+field tos bit8 6
+field totlen bit16 7
+field id bit16 8
+field flags bit3 9
+field fragoff bit13 10
+field ttl bit8 11
+field proto bit8 12
+field checksum bit16 13
+field srcAddr ipv4 14
+field dstAddr ipv4 15
+
+header tcp 3
+field srcPort bit16 16
+field dstPort bit16 17

tc/p4_tc_template.c
@@ -28,6 +28,7 @@
 #include "tc_common.h"
 #include "tc_util.h"
 #include "p4_types.h"
+#include "p4tc_introspection.h"
 
 #define TMPL_ARRAY_START_IDX 1
 #define PATH_OBJ_IDX 0
@@ -65,6 +66,48 @@ static void p4template_usage(void)
 		"\n");
 
 	exit(-1);
+}
+
+static int print_hdrfield(struct rtattr *tb, __u32 parser_id,
+			   __u32 hdrfield_id, FILE *f)
+{
+	struct rtattr *tb_nest[P4TC_HDRFIELD_MAX + 1];
+	struct p4tc_header_field_ty *hdr_ty;
+
+	parse_rtattr_nested(tb_nest, P4TC_HDRFIELD_MAX, tb);
+
+	if (parser_id)
+		print_uint(PRINT_ANY, "parserid", "parserid %u\n",
+			   parser_id);
+
+	if (hdrfield_id)
+		print_uint(PRINT_ANY, "hdrfieldid", "hdrfieldid %u\n",
+			   hdrfield_id);
+
+	if (tb_nest[P4TC_HDRFIELD_DATA]) {
+		struct p4_type_s *type;
+
+		hdr_ty = RTA_DATA(tb_nest[P4TC_HDRFIELD_DATA]);
+
+		type = get_p4type_byid(hdr_ty->datatype);
+		print_string(PRINT_ANY, "containertype", "container type %s\n",
+			     type->name);
+		print_uint(PRINT_ANY, "startbit", "startbit %u\n",
+			   hdr_ty->startbit);
+		print_uint(PRINT_ANY, "endbit", "endbit %u\n",
+			   hdr_ty->endbit);
+	}
+
+	if (tb_nest[P4TC_HDRFIELD_NAME]) {
+		const char *hdrfieldname;
+
+		hdrfieldname = RTA_DATA(tb_nest[P4TC_HDRFIELD_NAME]);
+
+		print_string(PRINT_ANY, "hdrfieldname", "Header field name %s\n",
+			     hdrfieldname);
+	}
+
+	return 0;
 }
 
 static int print_p4_key(struct rtattr *tb, void *arg)
@@ -230,6 +273,19 @@ static int print_table_instance_flush(struct nlmsghdr *n,
 	const __u32 *cnt = RTA_DATA(cnt_attr);
 
 	print_uint(PRINT_ANY, "ticount", "    table instance flush count %u",
+		   *cnt);
+	print_nl();
+
+	return 0;
+}
+
+static int print_hdrfield_flush(struct nlmsghdr *n,
+				struct rtattr *cnt_attr,
+				FILE *f)
+{
+	const __u32 *cnt = RTA_DATA(cnt_attr);
+
+	print_uint(PRINT_ANY, "count", "    header field flush count %u",
 		   *cnt);
 	print_nl();
 
@@ -437,6 +493,13 @@ static int print_p4tmpl_1(struct nlmsghdr *n, __u16 cmd, struct rtattr *arg,
 		}
 		break;
 	}
+	case P4TC_OBJ_HDR_FIELD:
+		ids = RTA_DATA(tb[P4TC_PATH]);
+		if (cmd == RTM_DELP4TEMPLATE && (n->nlmsg_flags & NLM_F_ROOT))
+			print_hdrfield_flush(n, tb[P4TC_COUNT], f);
+		else
+			print_hdrfield(tb[P4TC_PARAMS], ids[0], ids[1], f);
+		break;
 	default:
 		break;
 	}
@@ -566,6 +629,8 @@ static int get_obj_type(const char *str_obj_type)
 		return P4TC_OBJ_TABLE_CLASS;
 	else if (!strcmp(str_obj_type, "tinst"))
 		return P4TC_OBJ_TABLE_INST;
+	else if (!strcmp(str_obj_type, "hdrfield"))
+		return P4TC_OBJ_HDR_FIELD;
 
 	return -1;
 }
@@ -577,6 +642,103 @@ static int concat_cb_name(char *full_name, const char *cbname,
 }
 
 #define STR_IS_EMPTY(str) ((str)[0] == '\0')
+
+static int parse_hdrfield_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,
+			       char *p4tcpath[], int cmd, unsigned int *flags)
+{
+	__u32 pipeid = 0, parser_id = 0, hdrfield_id = 0;
+	struct p4tc_header_field_ty hdr_ty = {0};
+	struct hdrfield fields[32] = {0};
+	struct hdrfield *field = NULL;
+	struct rtattr *count = NULL;
+	char **argv = *argv_p;
+	int argc = *argc_p;
+	int num_fields = 0;
+	/* Parser instance id + header field id */
+	__u32 ids[2] = {0};
+	char *pname, *parser_name, *hdrname, *fieldname;
+	char full_hdr_name[HDRFIELDNAMSIZ];
+	struct rtattr *tail;
+
+	while (argc > 0) {
+		if (strcmp(*argv, "pipeid") == 0) {
+			NEXT_ARG();
+			if (get_u32(&pipeid, *argv, 10) < 0)
+				return -1;
+		} else if (strcmp(*argv, "parserid") == 0) {
+			NEXT_ARG();
+			if (get_u32(&parser_id, *argv, 10) < 0)
+				return -1;
+		} else if (strcmp(*argv, "hdrfieldid") == 0) {
+			NEXT_ARG();
+			if (get_u32(&hdrfield_id, *argv, 10) < 0)
+				return -1;
+		}
+
+		argv++;
+		argc--;
+	}
+
+	pname = p4tcpath[PATH_PNAME_IDX];
+	parser_name = p4tcpath[PATH_PARSERNAME_IDX];
+	hdrname = p4tcpath[PATH_HDRNAME_IDX];
+	fieldname = p4tcpath[PATH_HDRFIELDNAME_IDX];
+	if (pname && hdrname) {
+		num_fields = p4tc_get_header_fields(fields, pname, hdrname);
+		if (num_fields < 0)
+			return num_fields;
+	}
+
+	if (hdrname && fieldname) {
+		field = p4tc_find_hdrfield(fields, fieldname, num_fields);
+		if (!field) {
+			fprintf(stderr,
+				"Unable to find header field in introspection file\n");
+			return -1;
+		}
+
+		hdr_ty.datatype = field->ty->containid;
+		hdr_ty.startbit = field->startbit;
+		hdr_ty.endbit = field->endbit;
+
+		ids[1] = field->id;
+	} else if (hdrfield_id) {
+		ids[1] = hdrfield_id;
+	} else if (cmd != RTM_NEWP4TEMPLATE) {
+		*flags |= NLM_F_ROOT;
+	}
+
+	/* Always add count nest unless it's a dump */
+	if (!((*flags & NLM_F_ROOT) && cmd == RTM_GETP4TEMPLATE))
+		count = addattr_nest(n, MAX_MSG, 1);
+
+	if (parser_id)
+		ids[0] = parser_id;
+	addattr_l(n, MAX_MSG, P4TC_PATH, ids, sizeof(ids));
+
+	tail = addattr_nest(n, MAX_MSG, P4TC_PARAMS);
+	if (parser_name)
+		addattrstrz(n, MAX_MSG, P4TC_HDRFIELD_PARSER_NAME,
+			    parser_name);
+	if (fieldname) {
+		concat_cb_name(full_hdr_name, hdrname, fieldname,
+			       HDRFIELDNAMSIZ);
+		addattrstrz(n, MAX_MSG, P4TC_HDRFIELD_NAME, full_hdr_name);
+		if (cmd == RTM_NEWP4TEMPLATE) {
+			addattr_l(n, MAX_MSG, P4TC_HDRFIELD_DATA, &hdr_ty,
+				  sizeof(hdr_ty));
+		}
+	}
+	addattr_nest_end(n, tail);
+
+	if (count)
+		addattr_nest_end(n, count);
+
+	*argc_p = argc;
+	*argv_p = argv;
+
+	return pipeid;
+}
 
 static int parse_table_instance_data(int *argc_p, char ***argv_p,
 				     struct nlmsghdr *n, char *p4tcpath[],
@@ -1201,6 +1363,14 @@ static int p4tmpl_cmd(int cmd, unsigned int flags, int *argc_p,
 	case P4TC_OBJ_TABLE_INST:
 		pipeid = parse_table_instance_data(&argc, &argv, &req.n,
 						   p4tcpath, cmd, &flags);
+		if (pipeid < 0)
+			return -1;
+		req.t.pipeid = pipeid;
+
+		break;
+	case P4TC_OBJ_HDR_FIELD:
+		pipeid = parse_hdrfield_data(&argc, &argv, &req.n, p4tcpath,
+					     cmd, &flags);
 		if (pipeid < 0)
 			return -1;
 		req.t.pipeid = pipeid;

tc/p4_types.c
@@ -123,6 +123,19 @@ static int parse_p4t_be32_val(void *val, const char *arg, int base)
 	return 0;
 }
 
+static int parse_p4t_ipv4_val(void *val, const char *arg, int base)
+{
+	inet_prefix *newaddr = val;
+	inet_prefix iaddr;
+
+	if (get_prefix_1(&iaddr, (char *)arg, AF_INET))
+		return -1;
+
+	*newaddr = iaddr;
+
+	return 0;
+}
+
 static void print_p4t_u8_val(const char *name, void *val, __u8 bitstart,
 			     __u8 bitend, FILE *f)
 {
@@ -410,6 +423,26 @@ static struct p4_type_s nulstring_typ = {
 	.name = "nstrn"
 };
 
+static struct p4_type_s mac_typ = {
+	.containid = P4T_MACADDR,
+	.parse_p4t = NULL,
+	.print_p4t = NULL,
+	.bitsz = 48,
+	.startbit = 0,
+	.endbit = 47,
+	.name = "macaddr"
+};
+
+static struct p4_type_s ipv4_typ = {
+	.containid = P4T_IPV4ADDR,
+	.parse_p4t = parse_p4t_ipv4_val,
+	.print_p4t = NULL,
+	.bitsz = 32,
+	.startbit = 0,
+	.endbit = 31,
+	.name = "ipv4"
+};
+
 void register_p4_types(void)
 {
 	hlist_add_head(&u8_typ.hlist, &types_list);
@@ -426,6 +459,8 @@ void register_p4_types(void)
 	hlist_add_head(&be32_typ.hlist, &types_list);
 	hlist_add_head(&string_typ.hlist, &types_list);
 	hlist_add_head(&nulstring_typ.hlist, &types_list);
+	hlist_add_head(&mac_typ.hlist, &types_list);
+	hlist_add_head(&ipv4_typ.hlist, &types_list);
 }
 
 void unregister_p4_types(void)
@@ -444,4 +479,6 @@ void unregister_p4_types(void)
 	hlist_del(&be32_typ.hlist);
 	hlist_del(&string_typ.hlist);
 	hlist_del(&nulstring_typ.hlist);
+	hlist_del(&mac_typ.hlist);
+	hlist_del(&ipv4_typ.hlist);
 }

tc/p4tc_common.h
@@ -1,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#define TMPL_ARRAY_START_IDX 1
+#define PATH_OBJ_IDX 0
+#define PATH_PNAME_IDX 1
+#define PATH_CBNAME_IDX 2
+#define PATH_MNAME_IDX 3
+#define PATH_TBCNAME_IDX 3
+#define PATH_TINAME_IDX 4
+#define PATH_ANAME_IDX 3
+
+#define PATH_PARSERNAME_IDX 2
+#define PATH_HDRNAME_IDX 3
+#define PATH_HDRFIELDNAME_IDX 4
+
+#define MAX_PATH_COMPONENTS 5
+
+void parse_path(char *path, char **p4tcpath);
+int get_obj_type(const char *str_obj_type);
+
+#define STR_IS_EMPTY(str) ((str)[0] == '\0')
+
+int concat_cb_name(char *full_name, const char *cbname,
+		   const char *objname, size_t sz);

tc/p4tc_introspection.h
@@ -1,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __P4TC_INTROSPECTION_H__
+#define __P4TC_INTROSPECTION_H__
+#include <linux/pkt_cls.h>
+#include <linux/gen_stats.h>
+#include <linux/p4tc.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "p4_types.h"
+
+#define ENV_VAR "INTROSPECTION"
+
+int str_to_type(const char *type_str);
+
+struct hdrfield {
+	char name[TEMPLATENAMSZ];
+	struct p4_type_s *ty;
+	__u32 id;
+	__u16 startbit;
+	__u16 endbit;
+};
+
+struct hdrfield *p4tc_find_hdrfield(struct hdrfield fields[],
+				    const char *fieldname,
+				    __u32 num_fields);
+int p4tc_get_header_fields(struct hdrfield fields[], const char *pname,
+			   const char *hdrname);
+#endif

commit 044345dcbd924bb6c9c03e5218992c70c51c77fd

p4tc: Stop parsing action parameters before reaching next action
Make the parameter parsing function abort parsing in introspection file
when a new action line is parsed.

Fixes: 19af70f6d9c1

tc/p4_tc_introspection.c
@@ -307,6 +307,8 @@ int p4tc_get_act_params(struct p4_param_s params[], const char *pname,
 		scanned = sscanf(line, "%s %s %u %u[^\n]", param_name,
 				 type_str, pipeid, &param_id);
 
+		if (strcmp(param_name, "action") == 0)
+			break;
 		if (scanned != 4)
 			continue;
 