tammela/iproute2-p4tc-pub

commit fc551f2d1fa5d88b66c7c0bc478c83797f6f5789

tc: Add aid field to struct action_util
This field is necessary because we need to send the action ID to
the metact command in the kernel, so that it can find the action by its
ID in the kernel.

Co-developed-by: Jamal Hadi Salim <jhs@mojatatu.com>
Co-developed-by: Victor Nogueira <victor@mojatatu.com>
Co-developed-by: Pedro Tammela <pctammela@mojatatu.com>
Signed-off-by: Jamal Hadi Salim <jhs@mojatatu.com>
Signed-off-by: Victor Nogueira <victor@mojatatu.com>
Signed-off-by: Pedro Tammela <pctammela@mojatatu.com>

tc/m_action.c
@@ -23,6 +23,8 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <dlfcn.h>
+#include <dirent.h>
+#include <errno.h>
 
 #include "utils.h"
 #include "tc_common.h"
@@ -87,7 +89,102 @@ static int parse_noaopt(struct action_util *au, int *argc_p,
 	return -1;
 }
 
-static struct action_util *get_action_kind(char *str)
+struct action_util *get_action_byid(__u32 actid)
+{
+	struct action_util *a;
+
+	for (a = action_list; a; a = a->next) {
+		if (a->aid == actid)
+			return a;
+	}
+
+	return NULL;
+}
+
+void discover_actions(void)
+{
+	struct action_util *a;
+	static void *aBODY;
+	struct dirent *e;
+	char *dirpath;
+	char buf[128];
+	char buf2[256];
+	void *dlh;
+	size_t l;
+	DIR *d;
+
+	dirpath = getenv("TC_LIB_DIR");
+	if (!dirpath) {
+		dirpath = LIBDIR "/tc/";
+		return;
+	}
+
+	fprintf(stderr, "Loading actions from dir <%s> ...\n", dirpath);
+	d = opendir(dirpath);
+	if (!d) {		// Not openable?  Not much to do...
+		fprintf(stderr, "can't opendir $TC_LIB_DIR: %s\n",
+			strerror(errno));
+		return;
+	}
+
+	while ((e = readdir(d))) {
+		if ((e->d_name[0] == '.') &&
+		    ((e->d_name[1] == '\0') ||
+		     ((e->d_name[1] == '.') && (e->d_name[2] == '\0'))))
+			continue;
+
+		l = strlen(&e->d_name[0]);
+		if (l < 4)
+			continue;
+		if (strncmp(&e->d_name[0], "m_", 2))
+			continue;
+
+		if (strcmp(&e->d_name[l - 2], ".o") &&
+		    strcmp(&e->d_name[l - 3], ".so"))
+			continue;
+
+		memset(buf, 0, l+1);
+		strncpy(buf, e->d_name, l-2);
+
+		dlh = dlopen(buf, RTLD_LAZY | RTLD_GLOBAL);
+		if (dlh == NULL) {
+			dlh = aBODY;
+			if (dlh == NULL) {
+				dlh = aBODY = dlopen(NULL, RTLD_LAZY);
+				if (dlh == NULL) {
+					fprintf(stderr, "dlh == NULL\n");
+					continue;
+				}
+			}
+		}
+
+		memset(buf2, 0, sizeof(buf2));
+		snprintf(buf2, 12+l-2, "%s_action_util", &buf[2]);
+		a = dlsym(dlh, buf2);
+		if (!a) {
+			continue;
+		} else {
+			if(get_action_byid(a->aid))
+				continue;
+		}
+
+		a->next = action_list;
+		action_list = a;
+	}
+
+	closedir(d);
+}
+
+void print_known_actions(void)
+{
+	struct action_util *a;
+
+	for (a = action_list; a; a = a->next)
+		fprintf(stdout, "Action(%d): %s\n", a->aid, a->id);
+
+}
+
+struct action_util *get_action_kind(char *str)
 {
 	const char *dynact = "dyna";
 	static void *aBODY;

tc/m_bpf.c
@@ -214,6 +214,7 @@ static int bpf_print_opt(struct action_util *au, FILE *f, struct rtattr *arg)
 
 struct action_util bpf_action_util = {
 	.id		= "bpf",
+	.aid		= TCA_ACT_BPF,
 	.parse_aopt	= bpf_parse_opt,
 	.print_aopt	= bpf_print_opt,
 };

tc/m_connmark.c
@@ -144,6 +144,7 @@ static int print_connmark(struct action_util *au, FILE *f, struct rtattr *arg)
 
 struct action_util connmark_action_util = {
 	.id = "connmark",
+	.aid = TCA_ACT_CONNMARK,
 	.parse_aopt = parse_connmark,
 	.print_aopt = print_connmark,
 };

tc/m_csum.c
@@ -224,6 +224,7 @@ print_csum(struct action_util *au, FILE *f, struct rtattr *arg)
 
 struct action_util csum_action_util = {
 	.id = "csum",
+	.aid = TCA_ACT_CSUM,
 	.parse_aopt = parse_csum,
 	.print_aopt = print_csum,
 };

tc/m_ct.c
@@ -491,6 +491,7 @@ static int print_ct(struct action_util *au, FILE *f, struct rtattr *arg)
 
 struct action_util ct_action_util = {
 	.id = "ct",
+	.aid = TCA_ID_CT,
 	.parse_aopt = parse_ct,
 	.print_aopt = print_ct,
 };

tc/m_ctinfo.c
@@ -263,6 +263,7 @@ static int print_ctinfo(struct action_util *au, FILE *f, struct rtattr *arg)
 
 struct action_util ctinfo_action_util = {
 	.id = "ctinfo",
+	.aid = TCA_ID_CTINFO,
 	.parse_aopt = parse_ctinfo,
 	.print_aopt = print_ctinfo,
 };

tc/m_gact.c
@@ -217,6 +217,7 @@ print_gact(struct action_util *au, FILE *f, struct rtattr *arg)
 
 struct action_util gact_action_util = {
 	.id = "gact",
+	.aid = TCA_ACT_GACT,
 	.parse_aopt = parse_gact,
 	.print_aopt = print_gact,
 };

tc/m_gate.c
@@ -74,6 +74,7 @@ static int print_gate(struct action_util *au, FILE *f, struct rtattr *arg);
 
 struct action_util gate_action_util = {
 	.id = "gate",
+	.aid = TCA_ID_GATE,
 	.parse_aopt = parse_gate,
 	.print_aopt = print_gate,
 };

commit 6b09057fdbd9de59a533d0207af69486e0daf24b

tc: Create an action array as a fail switch
Create an action array, so whenever iproute2 cannot find an action .so,
we can find it through the action name array.

Fixes: fc551f2d1fa5

Signed-off-by: Victor Nogueira <victor@mojatatu.com>

tc/m_action.c
@@ -100,32 +100,43 @@ struct action_util *get_action_byid(__u32 actid)
 
 	return NULL;
 }
+const char *act_names[] = {
+	"bpf",
+	"connmark",
+	"csum",
+	"ctinfo",
+	"ct",
+	"ematch",
+	"estimator",
+	"gact",
+	"gate",
+	"ife",
+	"metact",
+	"gact",
+	"gate",
+	"ife",
+	"metact",
+	"mirred",
+	"mpls",
+	"nat",
+	"pedit",
+	"police",
+	"sample",
+	"simple",
+	"skbedit",
+	"skbmod",
+	"tunnel_key",
+	"vlan",
+};
 
-void discover_actions(void)
+static void discover_actions_by_dir(DIR *d)
 {
 	struct action_util *a;
-	static void *aBODY;
 	struct dirent *e;
-	char *dirpath;
 	char buf[128];
 	char buf2[256];
 	void *dlh;
 	size_t l;
-	DIR *d;
-
-	dirpath = getenv("TC_LIB_DIR");
-	if (!dirpath) {
-		dirpath = LIBDIR "/tc/";
-		return;
-	}
-
-	fprintf(stderr, "Loading actions from dir <%s> ...\n", dirpath);
-	d = opendir(dirpath);
-	if (!d) {		// Not openable?  Not much to do...
-		fprintf(stderr, "can't opendir $TC_LIB_DIR: %s\n",
-			strerror(errno));
-		return;
-	}
 
 	while ((e = readdir(d))) {
 		if ((e->d_name[0] == '.') &&
@@ -147,16 +158,8 @@ void discover_actions(void)
 		strncpy(buf, e->d_name, l-2);
 
 		dlh = dlopen(buf, RTLD_LAZY | RTLD_GLOBAL);
-		if (dlh == NULL) {
-			dlh = aBODY;
-			if (dlh == NULL) {
-				dlh = aBODY = dlopen(NULL, RTLD_LAZY);
-				if (dlh == NULL) {
-					fprintf(stderr, "dlh == NULL\n");
-					continue;
-				}
-			}
-		}
+		if (dlh == NULL)
+			continue;
 
 		memset(buf2, 0, sizeof(buf2));
 		snprintf(buf2, 12+l-2, "%s_action_util", &buf[2]);
@@ -171,8 +174,47 @@ void discover_actions(void)
 		a->next = action_list;
 		action_list = a;
 	}
+}
 
-	closedir(d);
+static void discover_actions_by_array(void)
+{
+	struct action_util *a;
+	char buf[256] = {0};
+	void *dlh;
+	int i;
+
+	for (i = 0; i < sizeof(act_names) / sizeof(char *); i++) {
+		sprintf(buf, "%s_action_util", act_names[i]);
+		dlh = dlopen(NULL, RTLD_LAZY);
+		if (dlh == NULL) {
+			fprintf(stderr, "dlh == NULL\n");
+			continue;
+		}
+
+		a = dlsym(dlh, buf);
+		if (!a) {
+			continue;
+		} else {
+			if(get_action_byid(a->aid))
+				continue;
+		}
+
+		a->next = action_list;
+		action_list = a;
+	}
+}
+
+void discover_actions(void)
+{
+	const char *dirpath = get_tc_lib();
+	DIR *d = NULL;
+
+	d = opendir(dirpath);
+	if (d) {
+		discover_actions_by_dir(d);
+		closedir(d);
+	}
+	discover_actions_by_array();
 }
 
 void print_known_actions(void)