tammela/iproute2-p4tc-pub

commit 26da41eb25501906b6812e7f31bbfc4bb93325a2

p4tc: Add metact commands to action templates and params to metact
This is the first step to migrating metact to action templates.

This commit changes action templates to, instead of having their "code"
represented by TC actions, have their "code" represented by metact
commands. After some delibaration and analysis we concluded that it's
architectually cleaner to have metact commands instead of TC actions,
since we were already planning on allowing action templates to only call
metact actions.
It also has a performance advantage, since with TC actions we'd have
every instance of the action template running the same TC actions with
the same index, which would cause locks dispute and consequently
performance penalties.

Fixes: ae29fc437688

Co-developed-by: Victor Nogueira <victor@mojatatu.com>
Co-developed-by: Jamal Hadi Salim <jhs@mojatatu.com>
Co-developed-by: Pedro Tammela <pctammela@mojatatu.com>
Signed-off-by: Victor Nogueira <victor@mojatatu.com>
Signed-off-by: Jamal Hadi Salim <jhs@mojatatu.com>
Signed-off-by: Pedro Tammela <pctammela@mojatatu.com>

tc/p4_tc_template.c
@@ -552,7 +552,6 @@ static int print_table_instance_flush(struct nlmsghdr *n,
 static int print_action_template(struct nlmsghdr *n, struct rtattr *arg,
 				 __u32 a_id, FILE *f)
 {
-	FILE *fp = (FILE *)arg;
 	struct rtattr *tb[P4TC_ACT_MAX + 1];
 
 	parse_rtattr_nested(tb, P4TC_ACT_MAX, arg);
@@ -572,11 +571,8 @@ static int print_action_template(struct nlmsghdr *n, struct rtattr *arg,
 		close_json_array(PRINT_JSON, NULL);
 	}
 
-	if (tb[P4TC_ACT_LIST]) {
-		print_nl();
-		print_string(PRINT_FP, NULL, "    Action list:\n", NULL);
-		tc_print_action(fp, tb[P4TC_ACT_LIST], 0);
-	}
+	if (tb[P4TC_ACT_METACT_LIST])
+		print_metact_cmds(f, tb[P4TC_ACT_METACT_LIST]);
 
 	return 0;
 }
@@ -979,10 +975,15 @@ static int parse_action_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,
 	char **argv = *argv_p;
 	int argc = *argc_p;
 	__u32 pipeid = 0, actid = 0;
-	int ret = 0;
+	int ret = 0, ins_cnt = 0;
 	char *pname, *actname, *cbname;
+	struct action_util *a;
 	struct rtattr *count;
 	struct rtattr *tail;
+
+	discover_actions();
+
+	a = get_action_byid(TCA_ID_METACT);
 
 	pname = p4tcpath[PATH_PNAME_IDX];
 	cbname = p4tcpath[PATH_CBNAME_IDX];
@@ -998,26 +999,46 @@ static int parse_action_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,
 		return -1;
 	}
 
+	register_kernel_metadata();
+
 	count = addattr_nest(n, MAX_MSG, 1);
 	tail = addattr_nest(n, MAX_MSG, P4TC_PARAMS);
 	while (argc > 0) {
 		if (strcmp(*argv, "pipeid") == 0) {
 			NEXT_ARG();
-			if (get_u32(&pipeid, *argv, 10) < 0)
-				return -1;
+			if (get_u32(&pipeid, *argv, 10) < 0) {
+				ret = -1;
+				goto unregister;
+			}
 		} else if (strcmp(*argv, "actid") == 0) {
 			NEXT_ARG();
-			if (get_u32(&actid, *argv, 10) < 0)
-				return -1;
+			if (get_u32(&actid, *argv, 10) < 0) {
+				ret = -1;
+				goto unregister;
+			}
+		} else if (strcmp(*argv, "cmd") == 0) {
+			ins_cnt = parse_commands(a, &argc, &argv);
+			if (ins_cnt < 0) {
+				ret = -1;
+				goto unregister;
+			}
 		} else {
-			if (parse_dyna(&argc, &argv, false, pname, full_actname, n) < 0)
-				return -1;
+			if (parse_dyna(&argc, &argv, false, pname, full_actname, n) < 0) {
+				ret = -1;
+				goto unregister;
+			}
 		}
 		argv++;
 		argc--;
 	}
 	if (!STR_IS_EMPTY(full_actname))
 		addattrstrz(n, MAX_MSG, P4TC_ACT_NAME, full_actname);
+
+	if (add_commands(n, ins_cnt, P4TC_ACT_METACT_LIST) < 0) {
+		ret = -1;
+		goto unregister;
+	}
+
 	addattr_nest_end(n, tail);
 	if (actid)
 		addattr32(n, MAX_MSG, P4TC_PATH, actid);
@@ -1025,10 +1046,15 @@ static int parse_action_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,
 		*flags |= NLM_F_ROOT;
 	addattr_nest_end(n, count);
 
+	ret = pipeid;
+
+unregister:
+	unregister_kernel_metadata();
+
 	*argc_p = argc;
 	*argv_p = argv;
 
-	return pipeid;
+	return ret;
 }
 
 static int parse_hdrfield_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,

include/uapi/linux/p4tc.h
@@ -200,7 +200,7 @@ enum {
 	P4TC_ACT_PARMS, /* nested params */
 	P4TC_ACT_OPT, /* action opt */
 	P4TC_ACT_TM, /* action tm */
-	P4TC_ACT_LIST, /* actions list */
+	P4TC_ACT_METACT_LIST, /* command list */
 	P4TC_ACT_ACTIVE, /* u8 */
 	P4TC_ACT_PAD,
 	__P4TC_ACT_MAX

include/uapi/linux/pkt_cls.h
@@ -141,6 +141,7 @@ enum tca_id {
 	TCA_ID_CT,
 	TCA_ID_GATE,
 	TCA_ID_DYN,
+	TCA_ID_METACT,
 	/* other actions go here */
 	__TCA_ID_MAX = 1023
 };

include/uapi/linux/tc_act/tc_metact.h
@@ -112,6 +112,7 @@ enum {
 	TCA_METACT_PARMS,
 	TCA_METACT_LIST,
 	TCA_METACT_ACT_NAME,
+	TCA_METACT_ACT_PARMS,
 	TCA_METACT_PAD,
 	__TCA_METACT_MAX
 };

tc/m_dyna.c
@@ -238,11 +238,6 @@ int parse_dyna(int *argc_p, char ***argv_p, bool in_act, char *pname,
 					fprintf(stderr, "Unknown state\n");
 					goto err_out;
 				}
-			} else if (strcmp(*argv, "action") == 0) {
-				if (parse_action(&argc, &argv, P4TC_ACT_LIST, n)) {
-					fprintf(stderr, "Illegal action\"\n");
-					return -1;
-				}
 			} else {
 				break;
 			}
@@ -376,64 +371,8 @@ int print_dyna_parms(struct rtattr *arg, FILE *f)
 	return 0;
 }
 
-static int print_dyna(struct action_util *au, FILE *f, struct rtattr *arg)
-{
-	FILE *fp = (FILE *)arg;
-	struct rtattr *tb[P4TC_ACT_MAX + 1];
-	struct tc_act_dyna *opt;
-
-	parse_rtattr_nested(tb, P4TC_ACT_MAX, arg);
-
-	if (tb[P4TC_ACT_NAME]) {
-		const char *name = RTA_DATA(tb[P4TC_ACT_NAME]);
-
-		print_string(PRINT_ANY, "kind", "%s ", name);
-		print_nl();
-	}
-
-	if (tb[P4TC_ACT_OPT] == NULL) {
-		fprintf(stderr, "Missing dyna parameters\n");
-		return -1;
-	}
-	opt = RTA_DATA(tb[P4TC_ACT_OPT]);
-
-	print_string(PRINT_FP, NULL, "%s\t", _SL_);
-	print_action_control(f, "action ", opt->action, " ");
-	print_nl();
-
-	if (tb[P4TC_ACT_PARMS]) {
-		print_string(PRINT_FP, NULL, "\t%s\n", "params: ");
-		open_json_array(PRINT_JSON, "params");
-		print_dyna_parms(tb[P4TC_ACT_PARMS], f);
-		close_json_array(PRINT_JSON, NULL);
-	}
-
-	if (tb[P4TC_ACT_LIST]) {
-		print_nl();
-		print_string(PRINT_FP, NULL, "    Action list:\n", NULL);
-		tc_print_action(fp, tb[P4TC_ACT_LIST], 0);
-	}
-
-	print_nl();
-	print_uint(PRINT_ANY, "index", "\t index %u", opt->index);
-	print_int(PRINT_ANY, "ref", " ref %d", opt->refcnt);
-	print_int(PRINT_ANY, "bind", " bind %d", opt->bindcnt);
-
-	if (show_stats) {
-		if (tb[P4TC_ACT_TM]) {
-			struct tcf_t *tm = RTA_DATA(tb[P4TC_ACT_TM]);
-
-			print_tm(f, tm);
-		}
-	}
-
-	print_nl();
-
-	return 0;
-}
-
 struct action_util dyna_action_util = {
 	.id = "dyna",
 	.parse_aopt = parse_dyna_cb,
-	.print_aopt = print_dyna,
+	.print_aopt = print_metact,
 };

commit 12436e6f6543d71e350157bd2cf53831662e3fdb

p4tc: Call fill_user_metadata when parsing action templates
Action templates might hold metact commands in them, that's why we need
to fill the user metadata in its code before calling the metact
command parsing function

Fixes: 26da41eb2550

Signed-off-by: Victor Nogueira <victor@mojatatu.com>

tc/m_metact.c
@@ -1549,21 +1549,6 @@ int add_commands(struct nlmsghdr *n, int ins_cnt, int tca_id)
 	return 0;
 }
 
-static int fill_user_metadata(struct p4_metat_s metadata[])
-{
-	int num_metadata;
-	int i;
-
-	num_metadata = p4tc_get_metadata(metadata);
-	if (num_metadata < 0)
-		return -1;
-
-	for (i = 0; i < num_metadata; i++)
-		register_new_metadata(&metadata[i]);
-
-	return 0;
-}
-
 static int parse_metact(struct action_util *a, int *argc_p, char ***argv_p,
 			int tca_id, struct nlmsghdr *n)
 {

tc/p4_tc_template.c
@@ -968,6 +968,21 @@ int concat_cb_name(char *full_name, const char *cbname,
 	return snprintf(full_name, sz, "%s/%s", cbname, objname) >= sz ? -1 : 0;
 }
 
+int fill_user_metadata(struct p4_metat_s metadata[])
+{
+	int num_metadata;
+	int i;
+
+	num_metadata = p4tc_get_metadata(metadata);
+	if (num_metadata < 0)
+		return -1;
+
+	for (i = 0; i < num_metadata; i++)
+		register_new_metadata(&metadata[i]);
+
+	return 0;
+}
+
 static int parse_action_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,
 			     char *p4tcpath[], int cmd, unsigned int *flags)
 {
@@ -976,6 +991,7 @@ static int parse_action_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,
 	int argc = *argc_p;
 	__u32 pipeid = 0, actid = 0;
 	int ret = 0, ins_cnt = 0;
+	struct p4_metat_s metadata[32];
 	char *pname, *actname, *cbname;
 	struct action_util *a;
 	struct rtattr *count;
@@ -1000,6 +1016,7 @@ static int parse_action_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,
 	}
 
 	register_kernel_metadata();
+	fill_user_metadata(metadata);
 
 	count = addattr_nest(n, MAX_MSG, 1);
 	tail = addattr_nest(n, MAX_MSG, P4TC_PARAMS);

tc/p4tc_common.h
@@ -57,6 +57,7 @@ int parse_commands(struct action_util *a, int *argc_p, char ***argv_p);
 int add_commands(struct nlmsghdr *n, int ins_cnt, int tca_id);
 int print_metact_cmds(FILE *f, struct rtattr *arg);
 int print_metact(struct action_util *au, FILE *f, struct rtattr *arg);
+int fill_user_metadata(struct p4_metat_s metadata[]);
 
 #define STR_IS_EMPTY(str) ((str)[0] == '\0')
 