tammela/iproute2-p4tc-pub

commit 180dd70e869d437d6fa0a4ce5b7fffbe500d07d6

p4tc: add template actions create, update, delete, get, flush and dump
This commit allows users to create, update, delete, get, flush and dump
action templates.

Action templates work as new "dynamic" action types "on the fly". For
example, imagine that we have the following action in a P4 program:

action ipv4_forward(bit<48> dstAddr,bit<8> port)
{
     standard_metadata.egress_spec = port;
     hdr.ethernet.srcAddr = hdr.ethernet.dstAddr;
     hdr.ethernet.dstAddr = dstAddr;
     hdr.ipv4.ttl = hdr.ipv4.ttl - 1;
}

which is invoked on a table match as such:

table ipv4_lpm {
        key = {
            hdr.ipv4.dstAddr: lpm;
        }

        actions = {
            ipv4_forward;
            drop;
            NoAction;
        }

        size = 1024;
}

We don't have an equivalent built in action in TC. So our goal is
to create this action "on the fly". We do so by introducing action
templates. The description of action templates will broken down in three
parts. The first one is  templating, which deals with the action template
creation and description. The second one is runtime, which deals with
action template instantiation and binding (specifically to filters).
The last one is "Other control commands", which deals with other
important control path commands, like delete, getting, flushing and
dumping.

___TEMPLATING___

The initialisation of action templates occurs in three stages.
The first one we call "template creation".

___TEMPLATE CREATION___

In this stage we issue the template creation command which specifies the
action template name, its parameters and the parameter types.
So for the ipv4_forward action, the template creation op would look
something like this:

tc p4template create action/ptables/ipv4_forward \
  param dstAddr type mac param port type u8

Note that in the template creation op we usually just specify the action
name, the parameters and their respective types. Also see that we specify

tc/m_action.c
@@ -228,20 +228,31 @@ void print_known_actions(void)
 
 struct action_util *get_action_kind(char *str)
 {
+	const char *dynact = "dyna";
 	static void *aBODY;
 	void *dlh;
 	char buf[256];
 	struct action_util *a;
+	int looked4dyna = 0;
 #ifdef CONFIG_GACT
 	int looked4gact = 0;
 restart_s:
 #endif
 	for (a = action_list; a; a = a->next) {
-		if (strcmp(a->id, str) == 0)
-			return a;
+		if (looked4dyna) {
+			if (strcmp(a->id, dynact) == 0)
+				return a;
+		} else {
+			if (strcmp(a->id, str) == 0)
+				return a;
+		}
 	}
 
-	snprintf(buf, sizeof(buf), "%s/m_%s.so", get_tc_lib(), str);
+	if (looked4dyna)
+		snprintf(buf, sizeof(buf), "%s/m_%s.so", get_tc_lib(), dynact);
+	else
+		snprintf(buf, sizeof(buf), "%s/m_%s.so", get_tc_lib(), str);
+
 	dlh = dlopen(buf, RTLD_LAZY | RTLD_GLOBAL);
 	if (dlh == NULL) {
 		dlh = aBODY;
@@ -252,24 +263,32 @@ restart_s:
 		}
 	}
 
-	snprintf(buf, sizeof(buf), "%s_action_util", str);
+	if (looked4dyna)
+		snprintf(buf, sizeof(buf), "%s_action_util", dynact);
+	else
+		snprintf(buf, sizeof(buf), "%s_action_util", str);
 	a = dlsym(dlh, buf);
 	if (a == NULL)
 		goto noexist;
 
 reg:
+	if (looked4dyna)
+		strcpy(a->id, str);
 	a->next = action_list;
 	action_list = a;
 	return a;
 
 noexist:
+	if (!looked4dyna) {
+		looked4dyna = 1;
+		goto restart_s;
 #ifdef CONFIG_GACT
-	if (!looked4gact) {
+	} else if (!looked4gact) {
 		looked4gact = 1;
 		strcpy(str, "gact");
 		goto restart_s;
-	}
 #endif
+	}
 	a = calloc(1, sizeof(*a));
 	if (a) {
 		strncpy(a->id, "noact", 15);

include/uapi/linux/pkt_cls.h
@@ -140,8 +140,9 @@ enum tca_id {
 	TCA_ID_MPLS,
 	TCA_ID_CT,
 	TCA_ID_GATE,
+	TCA_ID_DYN,
 	/* other actions go here */
-	__TCA_ID_MAX = 255
+	__TCA_ID_MAX = 1023
 };
 
 #define TCA_ID_MAX __TCA_ID_MAX

tc/Makefile
@@ -1,7 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0
 TCOBJ= tc.o tc_qdisc.o tc_class.o tc_filter.o tc_util.o tc_monitor.o \
        tc_exec.o m_police.o m_estimator.o m_action.o m_ematch.o \
-       emp_ematch.tab.o emp_ematch.lex.o p4_types.o p4_tc_template.o
+       emp_ematch.tab.o emp_ematch.lex.o p4_types.o p4_tc_template.o \
+       m_dyna.o
 
 include ../config.mk
 

tc/m_dyna.c
@@ -1,0 +1,439 @@
+/*
+ * m_dyna.c		Dynamic actions module
+ *
+ *		This program is free software; you can distribute it and/or
+ *		modify it under the terms of the GNU General Public License
+ *		as published by the Free Software Foundation; either version
+ *		2 of the License, or (at your option) any later version.
+ *
+ * Copyright (c) 2022, Mojatatu Networks
+ * Copyright (c) 2022, Intel Corporation.
+ * Authors:     Jamal Hadi Salim <jhs@mojatatu.com>
+ *              Victor Nogueira <victor@mojatatu.com>
+ *              Pedro Tammela <pctammela@mojatatu.com>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <linux/pkt_cls.h>
+#include <linux/if_ether.h>
+#include <asm-generic/errno-base.h>
+#include <errno.h>
+
+
+#include "utils.h"
+#include "rt_names.h"
+#include "tc_util.h"
+#include "tc_common.h"
+#include "p4tc_common.h"
+#include "p4_types.h"
+
+static void explain(void)
+{
+	fprintf(stderr,
+		"Usage:... actname <state STATE> [PARAM] <index INDEX> <CONTROL>\n"
+		"\tPARAM := [STRING] <type TYPE> <id U32NUM> <VALUE>\n"
+		"\tSTRING being an arbitrary string\n"
+		"\tSTATE := <active | inactive>\n"
+		"\tTYPE maybe any valid type, such u8, u16, u32 or ipv4\n"
+		"\tU32NUM being an arbitrary u32\n"
+		"\tVALUE being a value for the parameter\n"
+		"\tINDEX := optional index value used\n"
+		"\tCONTROL := reclassify|pipe|drop|continue|ok\n");
+}
+
+static void usage(void)
+{
+	explain();
+	exit(-1);
+}
+
+struct param {
+	char name[ACTPARAMNAMSIZ];
+	__u32 id;
+	__u32 type;
+};
+
+static int dyna_add_param(struct param *param, const char *value, bool in_act,
+			  struct nlmsghdr *n)
+{
+	int ret = 0;
+
+	addattrstrz(n, MAX_MSG, P4TC_ACT_PARAMS_NAME, param->name);
+	if (param->id)
+		addattr32(n, MAX_MSG, P4TC_ACT_PARAMS_ID, param->id);
+	if (param->type)
+		addattr32(n, MAX_MSG, P4TC_ACT_PARAMS_TYPE, param->type);
+
+	if (in_act) {
+		struct p4_type_value val;
+		struct p4_type_s *t;
+		void *new_value;
+		void *new_mask;
+		__u32 sz;
+
+		t = get_p4type_byid(param->type);
+		if (!t) {
+			fprintf(stderr, "Unknown param type %d\n", param->type);
+			return -1;
+		}
+		sz = t->bitsz >> 3;
+		new_value = calloc(1, sz);
+		if (!new_value)
+			return -1;
+		new_mask = calloc(1, sz);
+		if (!new_mask) {
+			ret = -1;
+			goto free_value;
+		}
+
+		val.value = new_value;
+		val.mask = new_mask;
+		if (t->parse_p4t &&
+		    t->parse_p4t(&val, value, 0) < 0) {
+			ret = -1;
+			goto free_mask;
+		}
+
+		addattr_l(n, MAX_MSG, P4TC_ACT_PARAMS_VALUE, new_value, sz);
+		addattr_l(n, MAX_MSG, P4TC_ACT_PARAMS_MASK, new_mask, sz);
+
+free_mask:
+		free(new_mask);
+free_value:
+		free(new_value);
+	}
+
+	return ret;
+}
+
+static int dyna_param_copy_name(char *dst_pname, char *src_pname)
+{
+	if (strnlen(src_pname, ACTPARAMNAMSIZ) == ACTPARAMNAMSIZ)
+		return -1;
+
+	strcpy(dst_pname, src_pname);
+
+	return 0;
+}
+
+static int dyna_parse_param(int *argc_p, char ***argv_p, bool in_act,
+			    int *parms_count, struct nlmsghdr *n)
+{
+	struct param param = {0};
+	char **argv = *argv_p;
+	int argc = *argc_p;
+	struct rtattr *tail2;
+
+	NEXT_ARG();
+	tail2 = addattr_nest(n, MAX_MSG, *parms_count | NLA_F_NESTED);
+	if (dyna_param_copy_name(param.name, *argv) < 0) {
+		fprintf(stderr, "Param name too big");
+		return -E2BIG;
+	}
+	/* After we get the param name, we can look for it in the P4 JSON file.
+	 * If the param is found, we can instrospect its type and ID.
+	 */
+	NEXT_ARG();
+	while (argc > 0) {
+		if (strcmp(*argv, "type") == 0) {
+			struct p4_type_s *t;
+			__u32 bitsz;
+
+			NEXT_ARG();
+			t = get_p4type_byarg(*argv, &bitsz);
+			if (!t) {
+				fprintf(stderr, "Invalid type %s\n", *argv);
+				return -1;
+			}
+			param.type = t->containid;
+		} else if (strcmp(*argv, "id") == 0) {
+			__u32 id;
+
+			NEXT_ARG();
+			if (get_u32(&id, *argv, 10)) {
+				fprintf(stderr, "Invalid id %s\n",
+					*argv);
+				return -1;
+			}
+			param.id = id;
+		} else {
+			break;
+		}
+		argv++;
+		argc--;
+	}
+
+	if (dyna_add_param(&param, *argv, in_act, n) < 0)
+		return -1;
+
+	addattr_nest_end(n, tail2);
+	(*parms_count)++;
+
+	*argc_p = argc;
+	*argv_p = argv;
+
+	return 0;
+}
+
+/* pname and act_name are not used by this function but are important for
+ * introspection.
+ */
+int parse_dyna(int *argc_p, char ***argv_p, bool in_act, char *pname,
+	     char *act_name, struct nlmsghdr *n)
+{
+	struct rtattr *tail = NULL;
+	struct tc_act_dyna sel = {0};
+	char **argv = *argv_p;
+	int parms_count = 1;
+	int argc = *argc_p;
+	int ok = 0;
+
+	/* After finding the action by using pname and act_name, one can
+	 * recover the parameters, if the action exists, for introspection.
+	 */
+	while (argc > 0) {
+		if (in_act) {
+			if (strcmp(*argv, "param") == 0) {
+				if (!tail)
+					tail = addattr_nest(n, MAX_MSG,
+							    P4TC_ACT_PARMS | NLA_F_NESTED);
+
+				if (dyna_parse_param(&argc, &argv, in_act,
+						   &parms_count, n) < 0)
+					goto err_out;
+
+				if (argc && strcmp(*argv, "param") == 0)
+					continue;
+			} else {
+				break;
+			}
+		} else {
+			if (strcmp(*argv, "param") == 0) {
+				if (!tail)
+					tail = addattr_nest(n, MAX_MSG,
+							    P4TC_ACT_PARMS | NLA_F_NESTED);
+
+				if (dyna_parse_param(&argc, &argv, in_act,
+						   &parms_count, n) < 0)
+					goto err_out;
+
+				if (argc && strcmp(*argv, "param") == 0)
+					continue;
+			} else if (strcmp(*argv, "state") == 0) {
+				NEXT_ARG();
+				if (strcmp(*argv, "active") == 0) {
+					addattr8(n, MAX_MSG, P4TC_ACT_ACTIVE,
+						 1);
+				} else if (strcmp(*argv, "inactive") == 0) {
+					addattr8(n, MAX_MSG, P4TC_ACT_ACTIVE,
+						 0);
+				} else {
+					fprintf(stderr, "Unknown state\n");
+					goto err_out;
+				}
+			} else if (strcmp(*argv, "action") == 0) {
+				if (parse_action(&argc, &argv, P4TC_ACT_LIST, n)) {
+					fprintf(stderr, "Illegal action\"\n");
+					return -1;
+				}
+			} else {
+				break;
+			}
+		}
+		argv++;
+		argc--;
+	}
+	if (tail)
+		addattr_nest_end(n, tail);
+
+	if (in_act)
+		parse_action_control_dflt(&argc, &argv, &sel.action, false,
+					  TC_ACT_PIPE);
+
+	if (argc) {
+		if (matches(*argv, "index") == 0) {
+			NEXT_ARG();
+			if (get_u32(&sel.index, *argv, 10)) {
+				fprintf(stderr, "simple: Illegal \"index\" (%s)\n",
+					*argv);
+				return -1;
+			}
+			ok += 1;
+			argc--;
+			argv++;
+		}
+	}
+
+	if (argc > 0) {
+		fprintf(stderr, "Unknown argument\n");
+		goto err_out;
+	}
+
+	if (in_act)
+		addattr_l(n, MAX_MSG, P4TC_ACT_OPT, &sel, sizeof(sel));
+
+	*argc_p = argc;
+	*argv_p = argv;
+
+	return 0;
+
+err_out:
+	usage();
+	return -1;
+}
+
+static int
+parse_dyna_cb(struct action_util *a, int *argc_p, char ***argv_p, int tca_id,
+	    struct nlmsghdr *n)
+{
+	char **argv = *argv_p;
+	int argc = *argc_p;
+	struct rtattr *tail;
+	int ret;
+
+	NEXT_ARG();
+	tail = addattr_nest(n, MAX_MSG, tca_id | NLA_F_NESTED);
+	ret = parse_dyna(&argc, &argv, true, NULL, a->id, n);
+	addattr_nest_end(n, tail);
+
+	*argc_p = argc;
+	*argv_p = argv;
+
+	return ret;
+}
+
+static int print_dyna_parm(FILE *f, struct rtattr *arg)
+{
+	struct rtattr *tb[P4TC_ACT_PARAMS_MAX + 1];
+	struct p4_type_s *t;
+
+	parse_rtattr_nested(tb, P4TC_ACT_PARAMS_MAX, arg);
+
+	if (tb[P4TC_ACT_PARAMS_NAME]) {
+		char *name;
+
+		name = RTA_DATA(tb[P4TC_ACT_PARAMS_NAME]);
+		print_string(PRINT_ANY, "name", "\t  %s ", name);
+	}
+
+	if (tb[P4TC_ACT_PARAMS_TYPE]) {
+		__u32 contain_id;
+
+		contain_id = *((__u32 *) RTA_DATA(tb[P4TC_ACT_PARAMS_TYPE]));
+		t = get_p4type_byid(contain_id);
+		if (!t) {
+			fprintf(stderr, "Unknown param type %d\n", contain_id);
+			return -1;
+		}
+
+		print_string(PRINT_ANY, "type", "type %s ", t->name);
+	} else {
+		fprintf(stderr, "Must specify params type");
+		return -1;
+	}
+
+	if (tb[P4TC_ACT_PARAMS_VALUE]) {
+		void *value = RTA_DATA(tb[P4TC_ACT_PARAMS_VALUE]);
+		void *mask = RTA_DATA(tb[P4TC_ACT_PARAMS_MASK]);
+		struct p4_type_value val;
+
+		val.value = value;
+		val.mask = mask;
+		if (t->print_p4t)
+			t->print_p4t(t->name, &val, f);
+	}
+
+	if (tb[P4TC_ACT_PARAMS_ID]) {
+		__u32 *id;
+
+		id = RTA_DATA(tb[P4TC_ACT_PARAMS_ID]);
+		print_uint(PRINT_ANY, "id", " id %u\n", *id);
+	}
+
+	return 0;
+}
+
+int print_dyna_parms(struct rtattr *arg, FILE *f)
+{
+	struct rtattr *tb[P4TC_MSGBATCH_SIZE + 1];
+	int i;
+
+	parse_rtattr_nested(tb, P4TC_MSGBATCH_SIZE, arg);
+
+	for (i = 1; i < P4TC_MSGBATCH_SIZE + 1 && tb[i]; i++) {
+		open_json_object(NULL);
+		print_dyna_parm(f, tb[i]);
+		close_json_object();
+	}
+
+	return 0;
+}
+
+static int print_dyna(struct action_util *au, FILE *f, struct rtattr *arg)
+{
+	FILE *fp = (FILE *)arg;
+	struct rtattr *tb[P4TC_ACT_MAX + 1];
+	struct tc_act_dyna *opt;
+
+	parse_rtattr_nested(tb, P4TC_ACT_MAX, arg);
+
+	if (tb[P4TC_ACT_NAME]) {
+		const char *name = RTA_DATA(tb[P4TC_ACT_NAME]);
+
+		print_string(PRINT_ANY, "kind", "%s ", name);
+		print_nl();
+	}
+
+	if (tb[P4TC_ACT_OPT] == NULL) {
+		fprintf(stderr, "Missing dyna parameters\n");
+		return -1;
+	}
+	opt = RTA_DATA(tb[P4TC_ACT_OPT]);
+
+	print_string(PRINT_FP, NULL, "%s\t", _SL_);
+	print_action_control(f, "action ", opt->action, " ");
+	print_nl();
+
+	if (tb[P4TC_ACT_PARMS]) {
+		print_string(PRINT_FP, NULL, "\t%s\n", "params: ");
+		open_json_array(PRINT_JSON, "params");
+		print_dyna_parms(tb[P4TC_ACT_PARMS], f);
+		close_json_array(PRINT_JSON, NULL);
+	}
+
+	if (tb[P4TC_ACT_LIST]) {
+		print_nl();
+		print_string(PRINT_FP, NULL, "    Action list:\n", NULL);
+		tc_print_action(fp, tb[P4TC_ACT_LIST], 0);
+	}
+
+	print_nl();
+	print_uint(PRINT_ANY, "index", "\t index %u", opt->index);
+	print_int(PRINT_ANY, "ref", " ref %d", opt->refcnt);
+	print_int(PRINT_ANY, "bind", " bind %d", opt->bindcnt);
+
+	if (show_stats) {
+		if (tb[P4TC_ACT_TM]) {
+			struct tcf_t *tm = RTA_DATA(tb[P4TC_ACT_TM]);
+
+			print_tm(f, tm);
+		}
+	}
+
+	print_nl();
+
+	return 0;
+}
+
+struct action_util dyna_action_util = {
+	.id = "dyna",
+	.parse_aopt = parse_dyna_cb,
+	.print_aopt = print_dyna,
+};

tc/p4_tc_template.c
@@ -37,6 +37,7 @@
 #define PATH_MNAME_IDX 3
 #define PATH_TBCNAME_IDX 3
 #define PATH_TINAME_IDX 4
+#define PATH_ANAME_IDX 3
 
 static int try_strncpy(char *dest, const char *src, size_t max_len)
 {
@@ -279,6 +280,51 @@ static int print_table_instance_flush(struct nlmsghdr *n,
 	return 0;
 }
 
+static int print_action_template(struct nlmsghdr *n, struct rtattr *arg,
+				 __u32 a_id, FILE *f)
+{
+	FILE *fp = (FILE *)arg;
+	struct rtattr *tb[P4TC_ACT_MAX + 1];
+
+	parse_rtattr_nested(tb, P4TC_ACT_MAX, arg);
+
+	if (tb[P4TC_ACT_NAME]) {
+		const char *name = RTA_DATA(tb[P4TC_ACT_NAME]);
+
+		print_string(PRINT_ANY, "aname", "    template action name %s\n", name);
+	}
+	if (a_id)
+		print_uint(PRINT_ANY, "actid", "    action id %u\n", a_id);
+
+	if (tb[P4TC_ACT_PARMS]) {
+		print_string(PRINT_FP, NULL, "\n\t params:\n", "");
+		open_json_array(PRINT_JSON, "params");
+		print_dyna_parms(tb[P4TC_ACT_PARMS], f);
+		close_json_array(PRINT_JSON, NULL);
+	}
+
+	if (tb[P4TC_ACT_LIST]) {
+		print_nl();
+		print_string(PRINT_FP, NULL, "    Action list:\n", NULL);
+		tc_print_action(fp, tb[P4TC_ACT_LIST], 0);
+	}
+
+	return 0;
+}
+
+static int print_action_template_flush(struct nlmsghdr *n,
+				       struct rtattr *cnt_attr,
+				       FILE *f)
+{
+	const __u32 *cnt = RTA_DATA(cnt_attr);
+
+	print_uint(PRINT_ANY, "count", "    action template flush count %u",
+		   *cnt);
+	print_nl();
+
+	return 0;
+}
+
 static int print_hdrfield_flush(struct nlmsghdr *n,
 				struct rtattr *cnt_attr,
 				FILE *f)
@@ -500,6 +546,18 @@ static int print_p4tmpl_1(struct nlmsghdr *n, __u16 cmd, struct rtattr *arg,
 		else
 			print_hdrfield(tb[P4TC_PARAMS], ids[0], ids[1], f);
 		break;
+	case P4TC_OBJ_ACT:
+		ids = RTA_DATA(tb[P4TC_PATH]);
+		if (cmd == RTM_DELP4TEMPLATE && (n->nlmsg_flags & NLM_F_ROOT))
+			print_action_template_flush(n, tb[P4TC_COUNT], f);
+		else {
+			if (tb[P4TC_PATH])
+				print_action_template(n, tb[P4TC_PARAMS],
+						      ids[0], f);
+			else
+				print_action_template(n, tb[P4TC_PARAMS], 0, f);
+		}
+		break;
 	default:
 		break;
 	}
@@ -576,6 +634,9 @@ int print_p4tmpl(struct nlmsghdr *n, void *arg)
 		print_string(PRINT_ANY, "obj", "templates obj type %s\n",
 			     "table instance");
 		break;
+	case P4TC_OBJ_ACT:
+		print_string(PRINT_ANY, "obj", "template obj type %s\n",
+			     "action template");
 	}
 
 	if (tb[P4TC_ROOT_PNAME]) {
@@ -631,6 +692,8 @@ static int get_obj_type(const char *str_obj_type)
 		return P4TC_OBJ_TABLE_INST;
 	else if (!strcmp(str_obj_type, "hdrfield"))
 		return P4TC_OBJ_HDR_FIELD;
+	else if (!strcmp(str_obj_type, "action"))
+		return P4TC_OBJ_ACT;
 
 	return -1;
 }
@@ -642,6 +705,66 @@ static int concat_cb_name(char *full_name, const char *cbname,
 }
 
 #define STR_IS_EMPTY(str) ((str)[0] == '\0')
+
+static int parse_action_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,
+			     char *p4tcpath[], int cmd, unsigned int *flags)
+{
+	char full_actname[ACTNAMSIZ] = {0};
+	char **argv = *argv_p;
+	int argc = *argc_p;
+	__u32 pipeid = 0, actid = 0;
+	int ret = 0;
+	char *pname, *actname, *cbname;
+	struct rtattr *count;
+	struct rtattr *tail;
+
+	pname = p4tcpath[PATH_PNAME_IDX];
+	cbname = p4tcpath[PATH_CBNAME_IDX];
+	actname = p4tcpath[PATH_ANAME_IDX];
+
+	if (cbname && actname)
+		ret = concat_cb_name(full_actname, cbname, actname, ACTNAMSIZ);
+	else if (cbname)
+		ret = try_strncpy(full_actname, cbname, ACTNAMSIZ);
+
+	if (ret < 0) {
+		fprintf(stderr, "Action name too long\n");
+		return -1;
+	}
+
+	count = addattr_nest(n, MAX_MSG, 1 | NLA_F_NESTED);
+	tail = addattr_nest(n, MAX_MSG, P4TC_PARAMS | NLA_F_NESTED);
+
+	while (argc > 0) {
+		if (strcmp(*argv, "pipeid") == 0) {
+			NEXT_ARG();
+			if (get_u32(&pipeid, *argv, 10) < 0)
+				return -1;
+		} else if (strcmp(*argv, "actid") == 0) {
+			NEXT_ARG();
+			if (get_u32(&actid, *argv, 10) < 0)
+				return -1;
+		} else {
+			if (parse_dyna(&argc, &argv, false, pname, full_actname, n) < 0)
+				return -1;
+		}
+		argv++;
+		argc--;
+	}
+	if (!STR_IS_EMPTY(full_actname))
+		addattrstrz(n, MAX_MSG, P4TC_ACT_NAME, full_actname);
+	addattr_nest_end(n, tail);
+	if (actid)
+		addattr32(n, MAX_MSG, P4TC_PATH, actid);
+	if (!actid && !cbname && !actname)
+		*flags |= NLM_F_ROOT;
+	addattr_nest_end(n, count);
+
+	*argc_p = argc;
+	*argv_p = argv;
+
+	return pipeid;
+}
 
 static int parse_hdrfield_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,
 			       char *p4tcpath[], int cmd, unsigned int *flags)
@@ -1372,6 +1495,14 @@ static int p4tmpl_cmd(int cmd, unsigned int flags, int *argc_p,
 	case P4TC_OBJ_HDR_FIELD:
 		pipeid = parse_hdrfield_data(&argc, &argv, &req.n, p4tcpath,
 					     cmd, &flags);
+		if (pipeid < 0)
+			return -1;
+		req.t.pipeid = pipeid;
+
+		break;
+	case P4TC_OBJ_ACT:
+		pipeid = parse_action_data(&argc, &argv, &req.n, p4tcpath, cmd,
+					&flags);
 		if (pipeid < 0)
 			return -1;
 		req.t.pipeid = pipeid;

tc/tc_common.h
@@ -13,6 +13,9 @@ int do_p4tmpl(int argc, char **argv);
 int do_tcmonitor(int argc, char **argv);
 int do_exec(int argc, char **argv);
 
+int parse_dyna(int *argc_p, char ***argv_p, bool in_act, char *pname,
+	     char *act_name, struct nlmsghdr *n);
+int print_dyna_parms(struct rtattr *arg, FILE *f);
 int print_p4tmpl(struct nlmsghdr *n, void *arg);
 int print_action(struct nlmsghdr *n, void *arg);
 int print_filter(struct nlmsghdr *n, void *arg);

tc/tc_util.h
@@ -57,7 +57,7 @@ struct filter_util {
 
 struct action_util {
 	struct action_util *next;
-	char id[FILTER_NAMESZ];
+	char id[ACTNAMSIZ];
 	__u32 aid;
 	int (*parse_aopt)(struct action_util *a, int *argc,
 			  char ***argv, int code, struct nlmsghdr *n);

commit 0434fb517d7c17eac6f7858e510033884f53f9c7

p4tc: Don't add dynamic action to action_list
All dynamic actions will have different names (field id) in struct
action_util. However they will always have the same struct action_util
pointer, since they all are created in m_dyna.c. This will cause a loop
whenever we add more than one dynamic struct action_util to the list.

That is

------------------
dyna_action_util |
------------------
 ^             |
 |             |
 ---------------
      next

The first dyna_action_util next pointer will be the same
dyna_action_util pointer, causing a loop.

Fixes: 180dd70e869d

Signed-off-by: Victor Nogueira <victor@mojatatu.com>

tc/m_action.c
@@ -272,10 +272,12 @@ restart_s:
 		goto noexist;
 
 reg:
-	if (looked4dyna)
+	if (looked4dyna) {
 		strcpy(a->id, str);
-	a->next = action_list;
-	action_list = a;
+	} else {
+		a->next = action_list;
+		action_list = a;
+	}
 	return a;
 
 noexist: