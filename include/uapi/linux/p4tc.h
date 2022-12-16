tammela/iproute2-p4tc-pub

commit 8c5a3389b6a089d17c1578180360b1325ec3f309

p4tc: Add table entry create, update, get, delete, flush and dump
This commit allows users to create, update, delete, get, flush and dump
table entries.

For example, a user issuing the following command:

    tc p4 create ptables/table/tname/tinst1 srcAddr 10.10.10.0/24 \
    dstAddr 192.168.0.0/16 prio 16

indicates a pipeline named "ptables" a table class "tname" and table
instance "tinst1".

User space tc will create a key which has a value of 0x0a0a0a00c0a00000
(10.10.10.0 concatenated with 192.168.0.0) and a mask value of
0xffffff00ffff0000 (/24 concatenated with /16) that will be sent to the
kernel. In addition a priority field of 16 is passed to the kernel.
The priority field is needed to disambiguate in case two entries
match. In that case, the kernel will choose the one with lowest priority
number.

Note that table entries can only be created once the pipeline template is
sealed.

If the user wanted to, for example, add an action to our just created
entry, they'd issue the following command:

tc p4 update ptables/table/tname/tinst1 srcAddr 10.10.10.0/24 \
dstAddr 192.168.0.0/16 prio 16 action ok

In this case, the user needs to specify the pipeline name, the table
type, the table instance, the keys and the priority, so that we can
locate the table entry.

If the user wanted to, for example, get the table entry that we just
updated, they'd issue the following command:

tc p4 get ptables/table/tname/tinst1 srcAddr 10.10.10.0/24 \
dstAddr 192.168.0.0/16 prio 16

Note that, again, we need to specify the pipeline name, the table
type, the table instance, the keys and the priority, so that we can
locate the table entry.

If the user wanted to delete the table entry we created, they'd issue the
following command:

tc p4 del ptables/table/tname/tinst1 srcAddr 10.10.10.0/24 \
dstAddr 192.168.0.0/16 prio 16

Note that, again, we need to specify the pipeline name, the table
type, the table instance, the keys and the priority, so that we can
locate the table entry.

lib/p4tc_names.c
@@ -1,0 +1,95 @@
+/*
+ * p4tc_names.c		P4 TC entity names DB.
+ *
+ *		This program is free software; you can distribute it and/or
+ *		modify it under the terms of the GNU General Public License
+ *		as published by the Free Software Foundation; either version
+ *		2 of the License, or (at your option) any later version.
+ *
+ * Copyright (c) 2022, Mojatatu Networks
+ * Copyright (c) 2022, Intel Corporation.
+ * Authors:     Jamal Hadi Salim <jhs@mojatatu.com>
+ *              Victor Nogueira <victor@mojatatu.com>
+ *              Pedro Tammela <pctammela@mojatatu.com>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <dirent.h>
+#include <limits.h>
+
+#include <asm/types.h>
+#include <linux/rtnetlink.h>
+
+#include <linux/p4tc_entities.h>
+
+#include "p4tc_names.h"
+#include "names.h"
+#include "utils.h"
+
+static char *p4tc_ctrlent_tab[256];
+
+static int p4tc_ctrlent_init;
+
+/* Not able to detect duplicate names, only ID */
+static void p4tc_ctrlent_initialise(void)
+{
+	struct dirent *de;
+	DIR *d;
+
+	p4tc_ctrlent_init = 1;
+	names_tab_initialize(CONFDIR "/p4tc_entities",
+			    p4tc_ctrlent_tab, 256);
+
+	d = opendir(CONFDIR "/p4tc_entities.d");
+	if (!d) {
+		fprintf(stderr, "Unable to open p4tc_entities dir\n");
+		return;
+	}
+
+	while ((de = readdir(d)) != NULL) {
+		char path[PATH_MAX];
+		size_t len;
+
+		if (*de->d_name == '.')
+			continue;
+
+		len = strlen(de->d_name);
+		if (len <= 5)
+			continue;
+		/* Must only consider files ending with ".conf" */
+		if (strcmp(de->d_name + len - 5, ".conf"))
+			continue;
+
+		snprintf(path, sizeof(path), CONFDIR "/p4tc_entities.d/%s",
+			 de->d_name);
+		names_tab_initialize(path, p4tc_ctrlent_tab, 256);
+	}
+	closedir(d);
+}
+
+/* Jamal: Should we return char *, or create some structure to put in the table?
+ */
+int p4tc_ctrltable_getbyid(__u8 id, char *str)
+{
+	if (!p4tc_ctrlent_init)
+		p4tc_ctrlent_initialise();
+
+	if (id >= 256) {
+		fprintf(stderr, "id must be smaller than 256");
+		return -1;
+	}
+
+	if (p4tc_ctrlent_tab[id]) {
+		strncpy(str, p4tc_ctrlent_tab[id], NAME_MAX_LEN);
+		return 0;
+	}
+
+	fprintf(stderr, "Control entity id not found\n");
+	return -1;
+}

etc/iproute2/p4tc_entities
@@ -1,0 +1,6 @@
+#
+# Reserved entities.
+#
+0	unspec
+1	kernel
+2	tc

etc/iproute2/p4tc_entities.d/README
@@ -1,0 +1,2 @@
+Each file in this directory is a p4tc_entities configuration file. iproute2
+commands scan this directory processing all files that end in '.conf'.

include/p4tc_names.h
@@ -1,0 +1,5 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#define NAME_MAX_LEN 512
+
+int p4tc_ctrltable_getbyid(__u8 id, char *str);

include/uapi/linux/rtnetlink.h
@@ -201,6 +201,13 @@ enum {
 	RTM_GETP4TEMPLATE,
 #define RTM_GETP4TEMPLATE	RTM_GETP4TEMPLATE
 
+	RTM_CREATEP4TBENT = 128,
+#define RTM_CREATEP4TBENT	RTM_CREATEP4TBENT
+	RTM_DELP4TBENT,
+#define RTM_DELP4TBENT		RTM_DELP4TBENT
+	RTM_GETP4TBENT,
+#define RTM_GETP4TBENT		RTM_GETP4TBENT
+
 	__RTM_MAX,
 #define RTM_MAX		(((__RTM_MAX + 3) & ~3) - 1)
 };

lib/Makefile
@@ -5,7 +5,7 @@ CFLAGS += -fPIC
 
 UTILOBJ = utils.o utils_math.o rt_names.o ll_map.o ll_types.o ll_proto.o ll_addr.o \
 	inet_proto.o namespace.o json_writer.o json_print.o json_print_math.o \
-	names.o color.o bpf_legacy.o bpf_glue.o exec.o fs.o cg_map.o
+	names.o color.o bpf_legacy.o bpf_glue.o exec.o fs.o cg_map.o p4tc_names.o
 
 ifeq ($(HAVE_ELF),y)
 ifeq ($(HAVE_LIBBPF),y)

tc/Makefile
@@ -2,7 +2,7 @@
 TCOBJ= tc.o tc_qdisc.o tc_class.o tc_filter.o tc_util.o tc_monitor.o \
        tc_exec.o m_police.o m_estimator.o m_action.o m_ematch.o \
        emp_ematch.tab.o emp_ematch.lex.o p4_types.o p4_tc_template.o \
-       m_dyna.o
+       m_dyna.o p4_tc_table.o p4_tc_introspection.o
 
 include ../config.mk
 

tc/p4_tc_introspection.c
@@ -101,7 +101,11 @@ static int p4tc_find_obj(FILE *f, const char *obj_name, __u32 *pipeid,
 	return -1;
 }
 
+#define find_table(f, obj_name, pipeid, objid) (p4tc_find_obj(f, obj_name, pipeid, objid, "table "))
+
 #define p4tc_find_header(f, obj_name, pipeid, objid) (p4tc_find_obj(f, obj_name, pipeid, &objid, "header "))
+
+#define p4tc_find_act(f, obj_name, pipeid, objid) (p4tc_find_obj(f, obj_name, pipeid, objid, "action "))
 
 int p4tc_get_header_fields(struct hdrfield fields[], const char *pname,
 			   const char *hdrname, __u32 *pipeid)
@@ -378,6 +382,112 @@ struct hdrfield *p4tc_find_hdrfield(struct hdrfield fields[],
 	for (i = 0; i < num_fields; i++) {
 		if (strncmp(fields[i].name, fieldname, TEMPLATENAMSZ) == 0)
 			return &fields[i];
+	}
+
+	return NULL;
+}
+
+int p4tc_get_tables(const char *pname, const char *tname, __u32 *pipeid,
+		    __u32 *tbcid)
+{
+	char path[PATH_MAX];
+	FILE *f;
+	int ret;
+
+	if (!getenv(ENV_VAR))
+		ret = snprintf(path, PATH_MAX, "%s/%s", INTROSPECTION_PATH,
+			       pname);
+	else
+		ret = snprintf(path, PATH_MAX, "%s/%s", getenv(ENV_VAR),
+			       pname);
+
+	if (ret < 0) {
+		fprintf(stderr, "Pipeline name too long\n");
+		return -1;
+	}
+
+	f = fopen(path, "r");
+	if (f == NULL) {
+		fprintf(stderr, "Unable to open introspection file\n");
+		return -1;
+	}
+
+	if (find_table(f, tname, pipeid, tbcid) < 0)
+		return -1;
+
+	return 0;
+}
+
+int p4tc_get_table_keys(struct tkey keys[], const char *pname,
+			const char *tname, __u32 tbc_id)
+{
+	int i = 0;
+	char *line = NULL;
+	__u32 pipeid = 0;
+	char key_name[TABLEKEYNAMSIZ];
+	char type_str[TCLASSNAMSIZ];
+	char path[PATH_MAX];
+	char key_str[4];
+	size_t len;
+	FILE *f;
+	int ret;
+
+	if (!pname)
+		return -1;
+
+	if (concat_pipeline_path(path, pname) < 0)
+		return -1;
+
+	f = fopen(path, "r");
+	if (f == NULL) {
+		fprintf(stderr, "Unable to open introspection file\n");
+		return -1;
+	}
+
+	if (find_table(f, tname, &pipeid, &tbc_id) < 0) {
+		ret = -1;
+		goto out;
+	}
+
+	while (getline(&line, &len, f) != -1) {
+		struct p4_type_s *p4_type;
+		int scanned;
+		__u32 bitsz;
+		__u32 key_id;
+
+		scanned = sscanf(line, "%s %s %s %u[^\n]", key_str, key_name,
+				 type_str, &key_id);
+		if (scanned != 4)
+			break;
+		if (strcmp(key_str, "key") != 0)
+			continue;
+
+		p4_type = get_p4type_byarg(type_str, &bitsz);
+		if (!p4_type)
+			return -1;
+		strncpy(keys[i].name, key_name, ACTPARAMNAMSIZ);
+		keys[i].type = p4_type;
+		keys[i].key_id = key_id;
+		i++;
+	}
+	ret = i;
+
+	fclose(f);
+	return ret;
+
+out:
+	fclose(f);
+	return ret;
+}
+
+struct tkey *p4tc_find_table_key(struct tkey keys[], const char *key_name,
+				 __u32 num_keys)
+{
+	int i;
+
+	for (i = 0; i < num_keys; i++) {
+		if (strncmp(keys[i].name, key_name, TABLEKEYNAMSIZ) == 0)
+			return &keys[i];
 	}
 
 	return NULL;

commit ec61011f439e01ac46f4f9529fcc7869c766e625

p4tc: Merge uapi file p4tc_entities.h into p4tc.h
To make debian happy and help in the vagrant build, lets merge
p4tc_entities.h into p4tc.h. This is also simplify the uapi header
files by removing one of them.

Fixes: 8c5a3389b6a0

Signed-off-by: Jamal Hadi Salim <jhs@mojatatu.com>

include/uapi/linux/p4tc.h
@@ -4,7 +4,6 @@
 
 #include <linux/types.h>
 #include <linux/pkt_sched.h>
-#include <linux/p4tc_entities.h>
 #include <linux/pkt_cls.h>
 
 /* pipeline header */
@@ -330,6 +329,13 @@ enum {
 	__P4TC_REGISTER_MAX
 };
 #define P4TC_REGISTER_MAX (__P4TC_REGISTER_MAX - 1)
+
+enum {
+	P4TC_ENTITY_UNSPEC,
+	P4TC_ENTITY_KERNEL,
+	P4TC_ENTITY_TC,
+	P4TC_ENTITY_MAX
+};
 
 #define P4TC_RTA(r)  ((struct rtattr *)(((char *)(r)) + NLMSG_ALIGN(sizeof(struct p4tcmsg))))
 

include/uapi/linux/p4tc_entities.h
@@ -1,9 +1,0 @@
-/* SPDX-License-Identifier: GPL-2.0 with Linux-syscall-note */
-#ifndef __LINUX_P4TC_ENTITIES_H
-#define __LINUX_P4TC_ENTITIES_H
-enum {
-	P4TC_ENTITY_UNSPEC,
-	P4TC_ENTITY_KERNEL,
-	P4TC_ENTITY_TC,
-};
-#endif

lib/p4tc_names.c
@@ -26,8 +26,6 @@
 #include <asm/types.h>
 #include <linux/rtnetlink.h>
 
-#include <linux/p4tc_entities.h>
-
 #include "p4tc_names.h"
 #include "names.h"
 #include "utils.h"