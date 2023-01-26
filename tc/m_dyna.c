tammela/iproute2-p4tc-pub

commit 8b83c9583cad08bd48681febe48e1ebe965ff342

p4tc: introducing dynamic action commands
In this initial patch, we introduce dynamic action commands which will be
the heart of P4TC.
Here we introduce 8 operations: set, act, print and branching

================================SET================================

The set operation allows us to assign values to objects. The assignee
object can be metadata, header field, table key, dev or register.
Whilst the assignor can be metadata, header field, table key, register,
constant, dev, param or result. We'll describe each of these object types
further down the commit message.

The set command has the following syntax:

set A B

Operand A's size must be bigger or equal to operand B's size.

Here are some examples of setting metadata to constants:

tc p4template create action/ptables/test \
cmd set metadata.kernel.skbmark constant.bit32.1234

tc p4template create action/ptables/test \
cmd metadata.kernel.tcindex constant.bit32.0x5678

The first example assigns kernel metadata skbmark to 1234 and the second
assigns kernel metadata tcindex to 0x5678

Note that we may specify constants in decimal or hexadecimal format.

Here are some examples of setting metadata to metadata:

tc p4template create action/ptables/test \
cmd set metadata.kernel.datalen metadata.kernel.skbmark

tc p4template create action/ptables/test \
cmd set metadata.kernel.skbifindex metadata.kernel.skbiif

We can also use user defined metadata in set operations.

For example, if we define the following user metadata

tc p4template create metadata/ptables/mymd type bit32

We could set its value to skbmark, for example

tc p4template create action/ptables/test \
cmd set metadata.ptables.mymd metadata.kernel.skbmark

Note that the way to reference user metadata is equivalent
to the way we reference kernel metadata. That is:

METADATA.PIPELINE_NAME.METADATA_NAME

All kernel metadata is stored inside a special pipeline called "kernel".

We can also use bit slices when in set operations. So, for example,

tc/m_dyna.c
@@ -33,6 +33,7 @@
 #include "tc_common.h"
 #include "p4tc_common.h"
 #include "p4_types.h"
+#include "p4tc_cmds.h"
 
 static void explain(void)
 {
@@ -321,6 +322,8 @@ static int print_dyna_parm_value(FILE *f, struct action_util *au,
 	if (tb[P4TC_ACT_PARAMS_VALUE_OPND]) {
 		print_string(PRINT_FP, NULL, "\n\t  value:\n", "");
 		open_json_object("value");
+		p4tc_cmds_print_operand("A", au,
+			tb[P4TC_ACT_PARAMS_VALUE_OPND], f);
 		close_json_object();
 		print_string(PRINT_FP, NULL, "\t", "");
 	} else {
@@ -442,7 +445,7 @@ static int print_dyna(struct action_util *au, FILE *f, struct rtattr *arg)
 	strlcpy(au->id, RTA_DATA(tb[P4TC_ACT_NAME]),
 		RTA_LENGTH(RTA_PAYLOAD(tb[P4TC_ACT_NAME])));
 
-	return 0;
+	return p4tc_print_cmds(f, au, tb[P4TC_ACT_CMDS_LIST]);
 }
 
 struct action_util dyna_action_util = {

tc/Makefile
@@ -2,7 +2,7 @@
 TCOBJ= tc.o tc_qdisc.o tc_class.o tc_filter.o tc_util.o tc_monitor.o \
        tc_exec.o m_police.o m_estimator.o m_action.o m_ematch.o \
        emp_ematch.tab.o emp_ematch.lex.o p4_types.o p4_tc_template.o \
-       m_dyna.o p4_tc_table.o p4_tc_introspection.o
+       p4_tc_cmds.o m_dyna.o p4_tc_table.o p4_tc_introspection.o
 
 include ../config.mk
 

tc/p4_tc_template.c
@@ -23,12 +23,290 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <dlfcn.h>
+#include <linux/p4tc.h>
 
 #include "utils.h"
 #include "tc_common.h"
 #include "tc_util.h"
 #include "p4tc_common.h"
 #include "p4_types.h"
+#include "p4tc_cmds.h"
+
+static struct hlist_head kernel_metadata_list = {};
+
+static struct p4_metat_s pktlen_meta = {
+	.id = P4TC_KERNEL_META_PKTLEN,
+	.containid = P4T_U32,
+	.startbit = 0,
+	.endbit = 31,
+	.name = "pktlen",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s datalen_meta = {
+	.id = P4TC_KERNEL_META_DATALEN,
+	.containid = P4T_U32,
+	.startbit = 0,
+	.endbit = 31,
+	.name = "datalen",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbmark_meta = {
+	.id = P4TC_KERNEL_META_SKBMARK,
+	.containid = P4T_U32,
+	.startbit = 0,
+	.endbit = 31,
+	.name = "skbmark",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s tcindex_meta = {
+	.id = P4TC_KERNEL_META_TCINDEX,
+	.containid = P4T_U16,
+	.startbit = 0,
+	.endbit = 15,
+	.name = "tcindex",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbhash_meta = {
+	.id = P4TC_KERNEL_META_SKBHASH,
+	.containid = P4T_U32,
+	.startbit = 0,
+	.endbit = 31,
+	.name = "skbhash",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbprio_meta = {
+	.id = P4TC_KERNEL_META_SKBPRIO,
+	.containid = P4T_U32,
+	.startbit = 0,
+	.endbit = 31,
+	.name = "skbprio",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s ifindex_meta = {
+	.id = P4TC_KERNEL_META_IFINDEX,
+	.containid = P4T_S32,
+	.startbit = 0,
+	.endbit = 31,
+	.name = "ifindex",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s iif_meta = {
+	.id = P4TC_KERNEL_META_SKBIIF,
+	.containid = P4T_DEV,
+	.startbit = 0,
+	.endbit = 31,
+	.name = "iif",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s protocol_meta = {
+	.id = P4TC_KERNEL_META_PROTOCOL,
+	.containid = P4T_BE16,
+	.startbit = 0,
+	.endbit = 15,
+	.name = "skbproto",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbptype_meta = {
+	.id = P4TC_KERNEL_META_PKTYPE,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 2,
+	.name = "skbptype",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbidf_meta = {
+	.id = P4TC_KERNEL_META_IDF,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 0,
+	.name = "skbidf",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbipsum_meta = {
+	.id = P4TC_KERNEL_META_IPSUM,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 1,
+	.name = "skbipsum",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbfclon_meta = {
+	.id = P4TC_KERNEL_META_FCLONE,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 1,
+	.name = "skbfclon",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbpeek_meta = {
+	.id = P4TC_KERNEL_META_PEEKED,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 0,
+	.name = "skbpeek",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skboook_meta = {
+	.id = P4TC_KERNEL_META_OOOK,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 0,
+	.name = "skboook",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s skbqmap_meta = {
+	.id = P4TC_KERNEL_META_QMAP,
+	.containid = P4T_U16,
+	.startbit = 0,
+	.endbit = 15,
+	.name = "skbqmap",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s ptypeoff_meta = {
+	.id = P4TC_KERNEL_META_PTYPEOFF,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 7,
+	.name = "ptypeoff",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s cloneoff_meta = {
+	.id = P4TC_KERNEL_META_CLONEOFF,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 7,
+	.name = "cloneoff",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s direction_meta = {
+	.id = P4TC_KERNEL_META_DIRECTION,
+	.containid = P4T_U8,
+	.startbit = 0,
+	.endbit = 0,
+	.name = "direction",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+static struct p4_metat_s ptclnoff_meta = {
+	.id = P4TC_KERNEL_META_PTCLNOFF,
+	.containid = P4T_U16,
+	.startbit = 0,
+	.endbit = 15,
+	.name = "ptclnoff",
+	.pipeid = 0,
+	.pname = "kernel",
+};
+
+struct p4_metat_s *get_meta_byname(const char *pname, const char *name)
+{
+	struct hlist_node *m, *tmp_child;
+
+	hlist_for_each_safe(m, tmp_child, &kernel_metadata_list) {
+		struct p4_metat_s *meta;
+
+		meta = container_of(m, struct p4_metat_s, hlist);
+		if (strcmp(meta->pname, pname) == 0 &&
+		    strnlen(name, METANAMSIZ) == strnlen(meta->name, METANAMSIZ) &&
+		    strncasecmp(meta->name, name, strlen(name)) == 0)
+			return meta;
+	}
+
+	return NULL;
+}
+
+struct p4_metat_s *get_meta_byid(const __u32 pipeid, const __u32 id)
+{
+	struct hlist_node *m, *tmp_child;
+
+	hlist_for_each_safe(m, tmp_child, &kernel_metadata_list) {
+		struct p4_metat_s *meta;
+
+		meta = container_of(m, struct p4_metat_s, hlist);
+		if (pipeid == meta->pipeid &&
+		    id == meta->id)
+			return meta;
+	}
+
+	return NULL;
+}
+
+void register_new_metadata(struct p4_metat_s *meta)
+{
+	hlist_add_head(&meta->hlist, &kernel_metadata_list);
+}
+
+void register_kernel_metadata(void)
+{
+	register_new_metadata(&pktlen_meta);
+	register_new_metadata(&datalen_meta);
+	register_new_metadata(&skbmark_meta);
+	register_new_metadata(&tcindex_meta);
+	register_new_metadata(&skbhash_meta);
+	register_new_metadata(&skbprio_meta);
+	register_new_metadata(&ifindex_meta);
+	register_new_metadata(&iif_meta);
+	register_new_metadata(&skbidf_meta);
+	register_new_metadata(&protocol_meta);
+	register_new_metadata(&skbipsum_meta);
+	register_new_metadata(&skbfclon_meta);
+	register_new_metadata(&skbpeek_meta);
+	register_new_metadata(&skboook_meta);
+	register_new_metadata(&skbqmap_meta);
+	register_new_metadata(&ptypeoff_meta);
+	register_new_metadata(&skbptype_meta);
+	register_new_metadata(&cloneoff_meta);
+	register_new_metadata(&ptclnoff_meta);
+	register_new_metadata(&direction_meta);
+}
+
+void unregister_kernel_metadata(void)
+{
+	struct hlist_node *n, *tmp_meta;
+
+	hlist_for_each_safe(n, tmp_meta, &kernel_metadata_list) {
+		struct p4_metat_s *meta;
+
+		meta = container_of(n, struct p4_metat_s, hlist);
+		hlist_del(&meta->hlist);
+	}
+}
 
 #include "p4tc_common.h"
 
@@ -348,6 +626,9 @@ static int print_action_template(struct nlmsghdr *n, struct rtattr *arg,
 		print_dyna_parms(&au, tb[P4TC_ACT_PARMS], f);
 		close_json_array(PRINT_JSON, NULL);
 	}
+
+	if (tb[P4TC_ACT_CMDS_LIST])
+		p4tc_print_cmds(f, &au, tb[P4TC_ACT_CMDS_LIST]);
 
 	return 0;
 }
@@ -688,10 +969,13 @@ static int parse_action_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,
 	char **argv = *argv_p;
 	int argc = *argc_p;
 	__u32 pipeid = 0, actid = 0;
-	int ret = 0;
+	struct action_util a = {0};
+	int ret = 0, ins_cnt = 0;
 	char *pname, *actname, *cbname;
 	struct rtattr *count;
 	struct rtattr *tail;
+
+	discover_actions();
 
 	pname = p4tcpath[PATH_PNAME_IDX];
 	cbname = p4tcpath[PATH_CBNAME_IDX];
@@ -703,6 +987,11 @@ static int parse_action_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,
 		ret = try_strncpy(full_actname, cbname, ACTNAMSIZ);
 
 	if (ret < 0) {
+		fprintf(stderr, "Action name too long\n");
+		return -1;
+	}
+
+	if (snprintf(a.id, ACTNAMSIZ, "%s/%s", pname, full_actname) == ACTNAMSIZ) {
 		fprintf(stderr, "Action name too long\n");
 		return -1;
 	}
@@ -719,15 +1008,25 @@ static int parse_action_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,
 			NEXT_ARG();
 			if (get_u32(&actid, *argv, 10) < 0)
 				return -1;
+		} else if (strcmp(*argv, "cmd") == 0) {
+			ins_cnt = p4tc_parse_cmds(&a, &argc, &argv);
+			if (ins_cnt < 0)
+				return -1;
 		} else {
 			if (parse_dyna(&argc, &argv, false, pname, full_actname, n) < 0)
 				return -1;
+			if (argc && strcmp(*argv, "cmd") == 0)
+				continue;
 		}
 		argv++;
 		argc--;
 	}
 	if (!STR_IS_EMPTY(full_actname))
 		addattrstrz(n, MAX_MSG, P4TC_ACT_NAME, full_actname);
+
+	if (p4tc_add_cmds(n, ins_cnt, P4TC_ACT_CMDS_LIST) < 0)
+		return -1;
+
 	addattr_nest_end(n, tail);
 	if (actid)
 		addattr32(n, MAX_MSG, P4TC_PATH, actid);
@@ -735,10 +1034,12 @@ static int parse_action_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,
 		*flags |= NLM_F_ROOT;
 	addattr_nest_end(n, count);
 
+	ret = pipeid;
+
 	*argc_p = argc;
 	*argv_p = argv;
 
-	return pipeid;
+	return ret;
 }
 
 static int parse_hdrfield_data(int *argc_p, char ***argv_p, struct nlmsghdr *n,

tc/p4tc_cmds.h
@@ -1,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _P4TC_CMDS_H_
+#define _P4TC_CMDS_H_
+
+#include "tc_util.h"
+
+int p4tc_parse_cmds(struct action_util *a, int *argc_p, char ***argv_p);
+int p4tc_add_cmds(struct nlmsghdr *n, int ins_cnt, int tca_id);
+int p4tc_print_cmds(FILE *f, struct action_util *au, struct rtattr *arg);
+int p4tc_cmds_print_operand(const char *ABC, struct action_util *au,
+			    struct rtattr *op_attr, FILE *f);
+
+#endif

tc/p4tc_common.h
@@ -110,6 +110,13 @@ struct p4_reg_s {
 	__u32 endbit;
 };
 
+struct p4_metat_s *get_meta_byname(const char *pname, const char *name);
+struct p4_metat_s *get_meta_byid(const __u32 pipeid, const __u32 id);
+void register_kernel_metadata(void);
+void unregister_kernel_metadata(void);
+void register_new_metadata(struct p4_metat_s *meta);
+void unregister_metadata(struct p4_metat_s *meta);
+int fill_user_metadata(struct p4_metat_s metadata[]);
 
 #define TABLEKEYNAMSIZ TEMPLATENAMSZ
 

commit a63fce00429a190a4fdb1e9f123bfd2745e66a2f

p4tc: Don't print dynamic action commands when dumping entry action
Don't print dynamic action commands when dumping entry action.

Fixes: 8b83c9583cad

Signed-off-by: Victor Nogueira <victor@mojatatu.com>

tc/m_dyna.c
@@ -445,7 +445,7 @@ static int print_dyna(struct action_util *au, FILE *f, struct rtattr *arg)
 	strlcpy(au->id, RTA_DATA(tb[P4TC_ACT_NAME]),
 		RTA_LENGTH(RTA_PAYLOAD(tb[P4TC_ACT_NAME])));
 
-	return p4tc_print_cmds(f, au, tb[P4TC_ACT_CMDS_LIST]);
+	return 0;
 }
 
 struct action_util dyna_action_util = {