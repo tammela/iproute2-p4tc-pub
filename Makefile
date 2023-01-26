tammela/iproute2-p4tc-pub

commit 50c30c6a8ad425923997366673d5ddc36042e6c9

p4tc: add introspection

Makefile
@@ -26,6 +26,7 @@ MANDIR?=$(DATADIR)/man
 ARPDDIR?=/var/lib/arpd
 KERNEL_INCLUDE?=/usr/include
 BASH_COMPDIR?=$(DATADIR)/bash-completion/completions
+INTROSPECTION_PATH?=$(CONFDIR)/introspection
 
 # Path to db_185.h include
 DBM_INCLUDE:=$(DESTDIR)/usr/include
@@ -39,7 +40,8 @@ endif
 
 DEFINES+=-DCONFDIR=\"$(CONFDIR)\" \
          -DNETNS_RUN_DIR=\"$(NETNS_RUN_DIR)\" \
-         -DNETNS_ETC_DIR=\"$(NETNS_ETC_DIR)\"
+         -DNETNS_ETC_DIR=\"$(NETNS_ETC_DIR)\" \
+         -DINTROSPECTION_PATH=\"$(INTROSPECTION_PATH)\"
 
 #options for AX.25
 ADDLIB+=ax25_ntop.o
@@ -105,6 +107,8 @@ install: all
 	install -m 0755 -d $(DESTDIR)$(HDRDIR)
 	@for i in $(SUBDIRS);  do $(MAKE) -C $$i install; done
 	install -m 0644 $(shell find etc/iproute2 -maxdepth 1 -type f) $(DESTDIR)$(CONFDIR)
+	install -m 0755 -d $(DESTDIR)$(INTROSPECTION_PATH)
+	install -m 0644 $(shell find etc/iproute2/introspection -type f) $(DESTDIR)$(INTROSPECTION_PATH)
 	install -m 0755 -d $(DESTDIR)$(BASH_COMPDIR)
 	install -m 0644 bash-completion/tc $(DESTDIR)$(BASH_COMPDIR)
 	install -m 0644 bash-completion/devlink $(DESTDIR)$(BASH_COMPDIR)

etc/iproute2/introspection/ptables
@@ -1,0 +1,49 @@
+action test 22 1
+param1 bit32 1
+
+action ipv4_forward 22 2
+nhopDstAddr macaddr 1
+numPort bit9 2
+
+action ipv4_drop 22 3
+nhopDstAddr macaddr 1
+
+table cb/tname 22 22
+key srcAddr ipv4 1
+key dstAddr ipv4 1
+
+table cb/tname2 22 2
+key srcPort bit16 1
+key dstPort bit16 1
+
+table cb/tname3 22 3
+key randomKey1 bit8 1
+key randomKey2 bit32 1
+key randomKey3 bit64 1
+
+header ethernet 22 1
+field dstAddr macaddr 1 1
+field srcAddr macaddr 2 1
+field type bit16 14 1
+
+header ipv4 22 2
+field version bit4 15 1
+field ihl bit4 5 1
+field tos bit8 6 1
+field totlen bit16 7 1
+field id bit16 8 1
+field flags bit3 9 1
+field fragoff bit13 10 1
+field ttl bit8 11 1
+field proto bit8 12 1
+field checksum bit16 13 1
+field srcAddr ipv4 3 1
+field dstAddr ipv4 4 1
+
+header tcp 22 3
+field srcPort bit16 16 1
+field dstPort bit16 17 1
+
+metadata mymd bit32 22 1
+metadata mytest bit16 22 2
+metadata mytest64 bit64 22 3

etc/iproute2/introspection/simple_l3_pna
@@ -1,0 +1,37 @@
+table tname 22 22
+key srcAddr ipv4 1
+
+table MainControlImpl/l3_match_rx 1 1
+key dstAddr ipv4 1
+
+header ethernet 1 1
+field dstAddr macaddr 1 1
+field srcAddr macaddr 2 1
+field type bit16 14
+
+header ipv4 1 2
+field version bit4 15 1
+field ihl bit4 5 1
+field tos bit8 6 1
+field totlen bit16 7 1
+field id bit16 8 1
+field flags bit3 9 1
+field fragoff bit13 10 1
+field ttl bit8 11 1
+field proto bit8 12 1
+field checksum bit16 13 1
+field srcAddr ipv4 3 1
+field dstAddr ipv4 4 1
+
+header tcp 22 3
+field srcPort bit16 16 1
+field dstPort bit16 17 1
+
+metadata mymd bit32 1 1
+metadata mytest bit16 1 2
+metadata mytest64 bit64 1 3
+metadata global/drop bit1 1 4
+metadata global/direction bit1 1 5
+
+action pna/send_to_port 1 1
+port dev 1 1

tc/p4_tc_introspection.c
@@ -1,0 +1,210 @@
+/*
+ * p4_tc_introspection.c	P4 TC Introspection
+ *
+ *		This program is free software; you can distribute it and/or
+ *		modify it under the terms of the GNU General Public License
+ *		as published by the Free Software Foundation; either version
+ *		2 of the License, or (at your option) any later version.
+ *
+ * Copyright (c) 2022, Mojatatu Networks
+ * Copyright (c) 2022, Intel Corporation.
+ * Authors:     Jamal Hadi Salim <jhs@mojatatu.com>
+ *              Victor Nogueira <victor@mojatatu.com>
+ *              Pedro Tammela <pctammela@mojatatu.com>
+ */
+
+#include "p4tc_introspection.h"
+#include <limits.h>
+#include <stdbool.h>
+#include <dirent.h>
+
+static char *get_introspection_path(void)
+{
+	if (!getenv(ENV_VAR))
+		return INTROSPECTION_PATH;
+
+	return getenv(ENV_VAR);
+}
+
+static int concat_pipeline_path(char *path, const char *pname)
+{
+	char *introspection_path = get_introspection_path();
+	int ret;
+
+	ret = snprintf(path, PATH_MAX, "%s/%s", introspection_path,
+		       pname);
+	if (ret < 0) {
+		fprintf(stderr, "Pipeline name too long\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+/* XXX: Introspection will do this properly.
+ * For now we are using our own format, see: file introspection/ptables
+ */
+static int p4tc_find_obj(FILE *f, const char *obj_name, __u32 *pipeid,
+			 __u32 *objid, const char *obj_type)
+{
+	const size_t tobj_len = strlen(obj_type);
+	char parsed_obj_name[TEMPLATENAMSZ];
+	size_t obj_name_len = 0;
+	char *line = NULL;
+	size_t len = 0;
+	char fmtstr[256] = {0};
+	__u32 parsed_pipeid, parsed_objid;
+
+	if (!obj_name && !*objid) {
+		fprintf(stderr, "Must specify object name or id\n");
+		return -1;
+	}
+
+	if (obj_name)
+		obj_name_len = strlen(obj_name);
+
+	strcpy(fmtstr, obj_type);
+	strcat(fmtstr, "%s %u %u");
+
+	while (getline(&line, &len, f) != -1) {
+		char *objline = strstr(line, obj_type);
+
+		if (!objline)
+			continue;
+		const size_t objline_len = strlen(objline);
+
+		if (objline_len < obj_name_len + tobj_len)
+			continue;
+
+		if (sscanf(objline, fmtstr, parsed_obj_name, &parsed_pipeid, &parsed_objid) != 3) {
+			fprintf(stderr, "Invalid file format\n");
+			return -1;
+		}
+
+		if ((!*pipeid || (*pipeid && parsed_pipeid == *pipeid)) &&
+		    *objid && parsed_objid == *objid)
+			return 0;
+
+		if (obj_name_len && strncmp(parsed_obj_name, obj_name, obj_name_len) == 0) {
+			if (*pipeid) {
+				if (*pipeid != parsed_pipeid)
+					continue;
+			} else {
+				*pipeid = parsed_pipeid;
+			}
+			if (!*objid)
+				*objid = parsed_objid;
+			return 0;
+		}
+	}
+
+	return -1;
+}
+
+#define find_table(f, obj_name, pipeid, objid) (p4tc_find_obj(f, obj_name, pipeid, objid, "table "))
+
+int p4tc_get_tables(const char *pname, const char *tname, __u32 *pipeid,
+		    __u32 *tbcid)
+{
+	char path[PATH_MAX];
+	FILE *f;
+	int ret;
+
+	if (!getenv(ENV_VAR))
+		ret = snprintf(path, PATH_MAX, "%s/%s", INTROSPECTION_PATH,
+			       pname);
+	else
+		ret = snprintf(path, PATH_MAX, "%s/%s", getenv(ENV_VAR),
+			       pname);
+
+	if (ret < 0) {
+		fprintf(stderr, "Pipeline name too long\n");
+		return -1;
+	}
+
+	f = fopen(path, "r");
+	if (f == NULL) {
+		fprintf(stderr, "Unable to open introspection file\n");
+		return -1;
+	}
+
+	if (find_table(f, tname, pipeid, tbcid) < 0)
+		return -1;
+
+	return 0;
+}
+
+int p4tc_get_table_keys(struct tkey keys[], const char *pname,
+			const char *tname, __u32 tbl_id)
+{
+	int i = 0;
+	char *line = NULL;
+	__u32 pipeid = 0;
+	char key_name[TABLEKEYNAMSIZ];
+	char type_str[TABLENAMSIZ];
+	char path[PATH_MAX];
+	char key_str[4];
+	size_t len;
+	FILE *f;
+	int ret;
+
+	if (!pname)
+		return -1;
+
+	if (concat_pipeline_path(path, pname) < 0)
+		return -1;
+
+	f = fopen(path, "r");
+	if (f == NULL) {
+		fprintf(stderr, "Unable to open introspection file\n");
+		return -1;
+	}
+
+	if (find_table(f, tname, &pipeid, &tbl_id) < 0) {
+		ret = -1;
+		goto out;
+	}
+
+	while (getline(&line, &len, f) != -1) {
+		struct p4_type_s *p4_type;
+		int scanned;
+		__u32 bitsz;
+		__u32 key_id;
+
+		scanned = sscanf(line, "%s %s %s %u[^\n]", key_str, key_name,
+				 type_str, &key_id);
+		if (scanned != 4)
+			break;
+		if (strcmp(key_str, "key") != 0)
+			continue;
+
+		p4_type = get_p4type_byarg(type_str, &bitsz);
+		if (!p4_type)
+			return -1;
+		strncpy(keys[i].name, key_name, ACTPARAMNAMSIZ);
+		keys[i].type = p4_type;
+		keys[i].key_id = key_id;
+		i++;
+	}
+	ret = i;
+
+	fclose(f);
+	return ret;
+
+out:
+	fclose(f);
+	return ret;
+}
+
+struct tkey *p4tc_find_table_key(struct tkey keys[], const char *key_name,
+				 __u32 num_keys)
+{
+	int i;
+
+	for (i = 0; i < num_keys; i++) {
+		if (strncmp(keys[i].name, key_name, TABLEKEYNAMSIZ) == 0)
+			return &keys[i];
+	}
+
+	return NULL;
+}

tc/p4tc_introspection.h
@@ -1,0 +1,29 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __P4TC_INTROSPECTION_H__
+#define __P4TC_INTROSPECTION_H__
+#include <linux/pkt_cls.h>
+#include <linux/gen_stats.h>
+#include <linux/p4tc.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "p4_types.h"
+#include "p4tc_common.h"
+
+#define ENV_VAR "INTROSPECTION"
+
+struct mask_ops { int (*parse)(struct parse_state *state, __u32 *offset,
+		     const char *argv);
+};
+
+int str_to_type(const char *type_str);
+struct tkey *p4tc_find_table_key(struct tkey keys[], const char *key_name,
+				 __u32 num_keys);
+int p4tc_get_table_keys(struct tkey keys[], const char *pname,
+			const char *tname, const __u32 tbl_id);
+
+int p4tc_get_tables(const char *pname, const char *tname, __u32 *pipeid,
+		    __u32 *tbcid);
+#endif

commit 85ed3fece1e03d1236cdc0a129ed12545605e36c

p4tc: Don't copy contents of introspection path
Don't copy contents of introspection path and delete txt introspection
file example.

Fixes: 50c30c6a8ad4

Signed-off-by: Victor Nogueira <victor@mojatatu.com>

Makefile
@@ -108,7 +108,6 @@ install: all
 	@for i in $(SUBDIRS);  do $(MAKE) -C $$i install; done
 	install -m 0644 $(shell find etc/iproute2 -maxdepth 1 -type f) $(DESTDIR)$(CONFDIR)
 	install -m 0755 -d $(DESTDIR)$(INTROSPECTION_PATH)
-	install -m 0644 $(shell find etc/iproute2/introspection -type f) $(DESTDIR)$(INTROSPECTION_PATH)
 	install -m 0755 -d $(DESTDIR)$(BASH_COMPDIR)
 	install -m 0644 bash-completion/tc $(DESTDIR)$(BASH_COMPDIR)
 	install -m 0644 bash-completion/devlink $(DESTDIR)$(BASH_COMPDIR)

etc/iproute2/introspection/ptables
@@ -1,49 +1,0 @@
-action test 22 1
-param1 bit32 1
-
-action ipv4_forward 22 2
-nhopDstAddr macaddr 1
-numPort bit9 2
-
-action ipv4_drop 22 3
-nhopDstAddr macaddr 1
-
-table cb/tname 22 22
-key srcAddr ipv4 1
-key dstAddr ipv4 1
-
-table cb/tname2 22 2
-key srcPort bit16 1
-key dstPort bit16 1
-
-table cb/tname3 22 3
-key randomKey1 bit8 1
-key randomKey2 bit32 1
-key randomKey3 bit64 1
-
-header ethernet 22 1
-field dstAddr macaddr 1 1
-field srcAddr macaddr 2 1
-field type bit16 14 1
-
-header ipv4 22 2
-field version bit4 15 1
-field ihl bit4 5 1
-field tos bit8 6 1
-field totlen bit16 7 1
-field id bit16 8 1
-field flags bit3 9 1
-field fragoff bit13 10 1
-field ttl bit8 11 1
-field proto bit8 12 1
-field checksum bit16 13 1
-field srcAddr ipv4 3 1
-field dstAddr ipv4 4 1
-
-header tcp 22 3
-field srcPort bit16 16 1
-field dstPort bit16 17 1
-
-metadata mymd bit32 22 1
-metadata mytest bit16 22 2
-metadata mytest64 bit64 22 3