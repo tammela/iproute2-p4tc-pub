tammela/iproute2-p4tc-pub

commit 26f401e75e7d33deb40885b3f364c2794e0b283f

p4tc: Add JSON introspection parsing code
Add JSON introspection parsing code which will parse the JSON
introspection files and obtain the necessary information from them as
required by the user.

Co-developed-by: Victor Nogueira <victor@mojatatu.com>
Signed-off-by: Victor Nogueira <victor@mojatatu.com>
Co-developed-by: Pedro Tammela <pctammela@mojatatu.com>
Signed-off-by: Pedro Tammela <pctammela@mojatatu.com>
Co-developed-by: Neha Singh <neha1.singh@intel.com>
Signed-off-by: Neha Singh <neha1.singh@intel.com>
Signed-off-by: Jamal Hadi Salim <jhs@mojatatu.com>

tc/p4_tc_json_infra.h
@@ -1,0 +1,98 @@
+/* Internal definitions for p4c json artifacts */
+
+#ifndef __JSON_INFRA_H__
+#define __JSON_INFRA_H__
+#include <stdbool.h>
+#include <linux/types.h>
+
+#define P4_TC_NAME_LEN 256
+#define ENV_VAR "INTROSPECTION"
+
+/* Macros for json file*/
+#define JSON_PIPELINE_ID "id"
+#define JSON_PIPELINE_NAME "pipeline_name"
+#define JSON_TABLE "tables"
+#define JSON_TABLE_LIST_TABLE_NAME "name"
+#define JSON_TABLE_LIST_TABLE_ID "id"
+#define JSON_TABLE_LIST_TABLE_SIZE "tentries"
+#define JSON_TABLE_LIST_TABLE_KEYS "keyfields"
+#define JSON_TABLE_LIST_TABLE_KEY_ID "keyid"
+#define JSON_TABLE_LIST_TABLE_KEY_SIZE "keysize"
+#define JSON_TABLE_LIST_TABLE_KEY_FIELD_ID "id" //keyfield id
+#define JSON_TABLE_LIST_TABLE_KEY_NAME "name"
+#define JSON_TABLE_LIST_TABLE_KEY_TYPE "type"
+#define JSON_TABLE_LIST_TABLE_KEY_MATCH_TYPE "match_type"
+#define JSON_TABLE_LIST_TABLE_KEY_WIDTH "bitwidth"
+#define JSON_TABLE_LIST_TABLE_KEY_MANDATORY "mandatory"
+#define JSON_TABLE_LIST_TABLE_ACTIONS "actions"
+#define JSON_TABLE_LIST_TABLE_ACTION_NAME "name"
+#define JSON_TABLE_LIST_TABLE_ACTION_SCOPE "action_scope"
+#define JSON_TABLE_LIST_TABLE_ACTION_ID "id"
+#define JSON_TABLE_LIST_TABLE_ACTION_DATA "params"
+#define JSON_TABLE_LIST_TABLE_ACTION_DATA_ID "id"
+#define JSON_TABLE_LIST_TABLE_ACTION_DATA_NAME "name"
+//#define JSON_TABLE_LIST_TABLE_ACTION_DATA_TYPE "type"
+#define JSON_TABLE_LIST_TABLE_ACTION_DATA_WIDTH "bitwidth"
+
+enum p4_tc_match_type {
+	P4_TC_MATCH_TYPE_EXACT = 0,
+	P4_TC_MATCH_TYPE_TERNARY,
+	P4_TC_MATCH_TYPE_LPM,
+	P4_TC_MATCH_TYPE_INVALID
+};
+
+struct key_fields_list {
+	int id;
+	char name[P4_TC_NAME_LEN];
+	char type[P4_TC_NAME_LEN];
+	int parser_instance; //XXX: we should be using the name instead
+	int width;
+	bool mandatory;
+	enum p4_tc_match_type match_type;
+	struct key_fields_list *next;
+};
+
+struct action_data {
+	int id;
+	char name[P4_TC_NAME_LEN];
+	char type[P4_TC_NAME_LEN];
+	int width;
+	struct action_data *next;
+};
+
+struct actions_list {
+	int id;
+	char name[P4_TC_NAME_LEN];
+	char action_scope[P4_TC_NAME_LEN];
+	int action_data_count;
+	struct action_data *data;
+	struct actions_list *next;
+};
+
+struct table {
+	char name[P4_TC_NAME_LEN];
+	int id;
+	int size;
+	__u32 ksize;
+	__u32 kid;
+	//XXX: Assumes only one key; we may have more.
+	int key_fields_count;
+	struct key_fields_list *key_fields;
+	int actions_count;
+	struct actions_list *actions;
+};
+
+struct table_list {
+	struct table table;
+	struct table_list *next;
+};
+
+struct p4_tc_pipeline {
+	char name[P4_TC_NAME_LEN];
+	__u32 id;
+	int mat_tables_count;
+	int metadata_count;
+	struct table_list *mat_tables;
+};
+
+#endif /* __JSON_INFRA_H__ */

tc/Makefile
@@ -1,7 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0
 TCOBJ= tc.o tc_qdisc.o tc_class.o tc_filter.o tc_util.o tc_monitor.o \
        tc_exec.o m_police.o m_estimator.o m_action.o m_ematch.o \
-       emp_ematch.tab.o emp_ematch.lex.o p4_types.o cJSON.o cjson_utils.o
+       emp_ematch.tab.o emp_ematch.lex.o p4_types.o p4_tc_json.o cJSON.o \
+       cjson_utils.o
 
 include ../config.mk
 

tc/p4_tc_json.h
@@ -1,0 +1,28 @@
+#ifndef __JSON_IMPORT_H__
+#define __JSON_IMPORT_H__
+
+#include "p4_tc_json_infra.h"
+
+#define FREE_LIST(head) \
+	{ \
+		void *temp; \
+		while (head) { \
+			temp = head->next; \
+			free(head); \
+			head = temp; \
+		} \
+	}
+
+struct p4_tc_pipeline *p4_tc_import_json(const char *pname);
+void p4_tc_print_pipeline(struct p4_tc_pipeline *pipeline, FILE *fp);
+void p4_tc_print_table(struct table *t, FILE *fp);
+void p4_tc_print_key_data(struct table *t, __u8 *k, __u8 *m, int blen, FILE *fp,
+			  const char *prefix);
+struct actions_list *p4tc_find_act(struct p4_tc_pipeline *pipe,
+				   const char *act_name);
+struct table *p4tc_find_table(struct p4_tc_pipeline *p, const char *tab_name);
+struct table *p4tc_find_table_byid(struct p4_tc_pipeline *p, int tab_id);
+struct key_fields_list *p4tc_find_table_keyfield(struct table *t, __u32 keyid,
+						 const char *key_field_name);
+
+#endif /*__JSON_IMPORT_H__*/

commit 79fe6b9884df4e1e9347d948e8f8d1401f61c391

p4tc: Don't expect key_id for table in introspection JSON
Remove requirement of legacy field "key_id" from table dictionary in
JSON file

Fixes: 26f401e75e7d

Signed-off-by: Victor Nogueira <victor@mojatatu.com>

tc/p4_tc_json.c
@@ -244,14 +244,6 @@ static int json_parse_table(cJSON *table_cjson, struct table *table)
 		return -1;
 	}
 
-	ret = cjson_get_int(table_cjson, JSON_TABLE_LIST_TABLE_KEY_ID, &keyid);
-	if (ret) {
-		fprintf(stderr, "Failed to parse table keyid:<%s> for:\n<%s>\n",
-			JSON_TABLE_LIST_TABLE_KEY_ID,
-			cJSON_Print(table_cjson));
-		return -1;
-	}
-
 	ret = cjson_get_int(table_cjson, JSON_TABLE_LIST_TABLE_SIZE, &size);
 	if (ret) {
 		fprintf(stderr, "Failed to parse table size:<%s> for:\n<%s>\n",

tc/p4_tc_json_infra.h
@@ -16,7 +16,6 @@
 #define JSON_TABLE_LIST_TABLE_ID "id"
 #define JSON_TABLE_LIST_TABLE_SIZE "tentries"
 #define JSON_TABLE_LIST_TABLE_KEYS "keyfields"
-#define JSON_TABLE_LIST_TABLE_KEY_ID "keyid"
 #define JSON_TABLE_LIST_TABLE_KEY_SIZE "keysize"
 #define JSON_TABLE_LIST_TABLE_KEY_FIELD_ID "id" //keyfield id
 #define JSON_TABLE_LIST_TABLE_KEY_NAME "name"