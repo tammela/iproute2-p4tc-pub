eb0668e2fd6262ea2bf2ff47d50b4eb9018106e4
Victor Nogueira <victor@mojatatu.com>
2023-06-30 03:01:40 +0000

    p4tc: introduce P4 classifier
    Introduce P4 classifier, which we'll use execute our P4 pipelines in the
    kernel. To use the P4 classifier you must specify a pipeline name that
    will be associated to this filter. For example, if we were to add a
    filter to ingress of network interface device eth0 and associate it to
    P4 pipeline pname we'd issue the following command:
    
    tc filter add dev lo parent ffff: protocol ip prio 6 p4 pname simple_l3_pna
    
    We could also associate an action with this filter, which would look
    something like this:
    
    tc filter add dev lo parent ffff: protocol ip prio 6 p4 \
        pname simple_l3_pna action ok
    
    The classifier itself has the following steps:
    
    ================================PARSING================================
    
    The pipeline must always have a parser associated with it.
    So, in this first step, the classifier will execute the parser and
    extact all the header fields that were specified in the templating
    phase.
    
    Also remember that a P4 program/pipeline only has one parser.
    
    ================================PREACTIONS================================
    
    After parsing, the classifier will execute the pipeline preactions,
    if there are any.
    
    Most of the time, the pipeline preactions will consist of a metact table
    apply command. Which will start the match action chain common to P4
    programs.
    
    The preactions will return a standard action code (TC_ACT_OK,
    TC_ACT_SHOT and etc). If the preaction returns TC_ACT_PIPE, we'll
    continue to the next step of the filter execution, otherwise it will
    stop executing the filter and return the op code.
    
    ================================POSTACTIONS================================
    
    After the pipeline preactions have executed and returned TC_ACT_PIPE,
    the filter will execute the pipeline postactions, if there any.
    
    Like the preactions, the postactions will return a standard action code.
    If the postaction returns TC_ACT_PIPE, we'll continue to the next step of
    the filter execution, otherwise it will stop executing the filter and
    return the op code.
    
    ===============================FILTERACTIONS===============================
    
    After the pipeline preactions have executed and returned TC_ACT_PIPE,
    the filter will execute the fitler actions, if any were associate with it.
    
    The filter will return the op code returned by this action.
    
    Co-developed-by: Victor Nogueira <victor@mojatatu.com>
    Signed-off-by: Victor Nogueira <victor@mojatatu.com>
    Co-developed-by: Pedro Tammela <pctammela@mojatatu.com>
    Signed-off-by: Pedro Tammela <pctammela@mojatatu.com>
    Signed-off-by: Jamal Hadi Salim <jhs@mojatatu.com>

---
+++ tc/f_p4.c
@@ -0,0 +1,141 @@
+/*
+ * f_p4.c		P4 pipeline Classifier
+ *
+ *		This program is free software; you can distribute it and/or
+ *		modify it under the terms of the GNU General Public License
+ *		as published by the Free Software Foundation; either version
+ *		2 of the License, or (at your option) any later version.
+ *
+ * Authors:
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <linux/if.h>
+
+#include "utils.h"
+#include "tc_util.h"
+
+static void explain(void)
+{
+	fprintf(stderr,
+		"Usage: ... p4 \n"
+		"                 pname PNAME [ action ACTION_SPEC ] [ classid CLASSID ]\n"
+		"       ACTION_SPEC := ... look at individual actions\n"
+		"\n"
+		"NOTE: CLASSID is parsed as hexadecimal input.\n");
+}
+
+static int p4_parse_opt(struct filter_util *qu, char *handle,
+			   int argc, char **argv, struct nlmsghdr *n)
+{
+	struct tcmsg *t = NLMSG_DATA(n);
+	char *pname = NULL;
+	long h = 0;
+	struct rtattr *tail;
+
+	if (handle) {
+		h = strtol(handle, NULL, 0);
+		if (h == LONG_MIN || h == LONG_MAX) {
+			fprintf(stderr, "Illegal handle \"%s\", must be numeric.\n",
+			    handle);
+			return -1;
+		}
+	}
+	t->tcm_handle = h;
+
+	if (argc == 0)
+		return 0;
+
+	tail = addattr_nest(n, MAX_MSG, TCA_OPTIONS | NLA_F_NESTED);
+
+	while (argc > 0) {
+		if (strcmp(*argv, "classid") == 0 ||
+		    strcmp(*argv, "flowid") == 0) {
+			unsigned int handle;
+
+			NEXT_ARG();
+			if (get_tc_classid(&handle, *argv)) {
+				fprintf(stderr, "Illegal \"classid\"\n");
+				return -1;
+			}
+			addattr32(n, MAX_MSG, TCA_P4_CLASSID, handle);
+		} else if (strcmp(*argv, "action") == 0) {
+			NEXT_ARG();
+			if (parse_action(&argc, &argv, TCA_P4_ACT | NLA_F_NESTED, n)) {
+				fprintf(stderr, "Illegal \"action\"\n");
+				return -1;
+			}
+			continue;
+		} else if (strcmp(*argv, "pname") == 0) {
+
+			NEXT_ARG();
+
+			pname = *argv;
+			addattrstrz(n, MAX_MSG, TCA_P4_PNAME, *argv);
+
+		} else if (strcmp(*argv, "help") == 0) {
+			explain();
+			return -1;
+		} else {
+			fprintf(stderr, "What is \"%s\"?\n", *argv);
+			explain();
+			return -1;
+		}
+		argc--; argv++;
+	}
+	addattr_nest_end(n, tail);
+
+	if (!pname) {
+		fprintf(stderr, "pname MUST be provided\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+static int p4_print_opt(struct filter_util *qu, FILE *f,
+			   struct rtattr *opt, __u32 handle)
+{
+	struct rtattr *tb[TCA_P4_MAX+1];
+
+	if (opt == NULL)
+		return 0;
+
+	parse_rtattr_nested(tb, TCA_P4_MAX, opt);
+
+	if (handle)
+		print_uint(PRINT_ANY, "handle", "handle 0x%x ", handle);
+
+	if (tb[TCA_P4_CLASSID]) {
+		SPRINT_BUF(b1);
+		print_string(PRINT_ANY, "flowid", "flowid %s ",
+			sprint_tc_classid(rta_getattr_u32(tb[TCA_P4_CLASSID]),
+					  b1));
+	}
+
+	if (tb[TCA_P4_PNAME]) {
+		print_string(PRINT_ANY, "pname", "pname %s ",
+			     RTA_DATA(tb[TCA_P4_PNAME]));
+	} else {
+		print_string(PRINT_ANY, "pname", "pname %s ", "???");
+	}
+
+	if (tb[TCA_P4_ACT])
+		tc_print_action(f, tb[TCA_P4_ACT], 0);
+
+	return 0;
+}
+
+struct filter_util p4_filter_util = {
+	.id = "p4",
+	.parse_fopt = p4_parse_opt,
+	.print_fopt = p4_print_opt,
+};
